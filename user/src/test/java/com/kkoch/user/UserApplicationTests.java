101126cea20bb182d79b64d98438bb6fbde0f846
임우택 <lyt1228@naver.com>
2023-07-18 10:41:29 +0900

    🎉feat: setup kkoch user project

---
+++ user/src/test/java/com/kkoch/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkoch.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
