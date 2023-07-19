d21ed3c8dff7dc01c9eced5bbe3ff551d0b59573
홍승준 <ghdgn22@gmail.com>
2023-07-19 16:02:08 +0900

    📝docs: S09P12C204-132 알림 REST docs 수정

--- user/src/main/java/com/kkoch/user/api/controller/alarm/AlarmController.java
+++ user/src/main/java/com/kkoch/user/api/controller/alarm/AlarmController.java
@@ -1,6 +1,5 @@
 package com.kkoch.user.api.controller.alarm;
 
-
 import com.kkoch.user.api.controller.ApiResponse;
 import com.kkoch.user.api.controller.alarm.response.AlarmResponse;
 import io.swagger.annotations.Api;
@@ -11,38 +10,32 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.springframework.http.HttpStatus.MOVED_PERMANENTLY;
+
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/user-service")
+@RequestMapping("/user-service/alarms")
 @Slf4j
 @Api(tags = {"알림 기능"})
 public class AlarmController {
-
-    // 알림 조회
-    @ApiOperation("알림 단건 조회")
-    @GetMapping("/alarms/{id}")
-    public ApiResponse<AlarmResponse> getAlarm(@PathVariable Long id){
-        return ApiResponse.ok(null);
-    }
-
     // 알림 목록 조회
     @ApiOperation("알림 목록 조회")
-    @GetMapping("/alarms")
-    public ApiResponse<List<AlarmResponse>> getAlarmList(){
+    @GetMapping
+    public ApiResponse<List<AlarmResponse>> getAlarms() {
         return ApiResponse.ok(null);
     }
 
     // 읽기 처리
     @ApiOperation("알림 확인")
-    @PatchMapping("/alarms/{id}")
-    public ApiResponse<?> setCheckAlarm(@PathVariable Long id){
+    @PatchMapping("/{alarmId}")
+    public ApiResponse<?> setCheckAlarm(@PathVariable Long alarmId) {
         return ApiResponse.ok(null);
     }
 
     // 알림 삭제
     @ApiOperation("알림 삭제")
-    @DeleteMapping("/alarms/{id}")
-    public ApiResponse<?> removeAlaram(@PathVariable Long id){
-        return ApiResponse.ok(null);
+    @DeleteMapping("/{alarmId}")
+    public ApiResponse<?> removeAlaram(@PathVariable Long alarmId) {
+        return ApiResponse.of(MOVED_PERMANENTLY, "알림 삭제", null);
     }
 }
--- user/src/main/java/com/kkoch/user/api/controller/alarm/response/AlarmResponse.java
+++ user/src/main/java/com/kkoch/user/api/controller/alarm/response/AlarmResponse.java
@@ -2,15 +2,10 @@ package com.kkoch.user.api.controller.alarm.response;
 
 import lombok.Data;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-
 @Data
 public class AlarmResponse {
-
-
-    Long id;
-    String content;
-    boolean open;
-    String create_date;
+    private Long id;
+    private String content;
+    private boolean open;
+    private String createDate;
 }
