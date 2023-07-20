5d98367da1bc5631aca8ac3fafb88effa6e4f004
leeyr0412 <leeyr0412@naver.com>
2023-07-20 11:14:33 +0900

    🎉feat: S09P12C204-108 Auction Entity 생성

--- admin/src/main/java/com/kkoch/admin/domain/auction/Auction.java
+++ admin/src/main/java/com/kkoch/admin/domain/auction/Auction.java
@@ -1,4 +1,51 @@
 package com.kkoch.admin.domain.auction;
 
-public class Auction {
+import com.kkoch.admin.domain.TimeBaseEntity;
+import com.kkoch.admin.domain.admin.Admin;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import java.time.LocalDateTime;
+
+import static javax.persistence.EnumType.STRING;
+import static javax.persistence.FetchType.LAZY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Auction extends TimeBaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "auction_id")
+    private Long id;
+
+    private int code;
+    private LocalDateTime startTime;
+    @Column(nullable = false)
+    private boolean active;
+    @Column(length = 100)
+    private String title;
+    @Enumerated(STRING)
+    @Column(length = 20)
+    private Status status;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "admin_id")
+    private Admin admin;
+
+    @Builder
+    public Auction(Long id, int code, LocalDateTime startTime, boolean active, String title, Status status, Admin admin) {
+        this.id = id;
+        this.code = code;
+        this.startTime = startTime;
+        this.active = active;
+        this.title = title;
+        this.status = status;
+        this.admin = admin;
+    }
 }
---
+++ admin/src/main/java/com/kkoch/admin/domain/auction/Status.java
@@ -0,0 +1,5 @@
+package com.kkoch.admin.domain.auction;
+
+public enum Status {
+    READY, OPEN, CLOSE
+}
