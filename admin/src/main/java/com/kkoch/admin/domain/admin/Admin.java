31e92549bd0dac147c2cec9ef24ddeb06db9bcdd
임우택 <lyt1228@naver.com>
2023-07-20 10:59:33 +0900

    🎉feat: S09P12C204-72 Admin 도메인 설계

--- admin/src/main/java/com/kkoch/admin/domain/admin/Admin.java
+++ admin/src/main/java/com/kkoch/admin/domain/admin/Admin.java
@@ -1,4 +1,48 @@
 package com.kkoch.admin.domain.admin;
 
-public class Admin {
+import com.kkoch.admin.domain.TimeBaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Admin extends TimeBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "admin_id")
+    private Long id;
+
+    @Column(unique = true, nullable = false, updatable = false, length = 20)
+    private String loginId;
+
+    @Column(nullable = false, length = 60)
+    private String loginPw;
+
+    @Column(nullable = false, updatable = false, length = 20)
+    private String name;
+
+    @Column(unique = true, nullable = false, length = 13)
+    private String tel;
+
+    @Column(nullable = false, length = 2)
+    private String position;
+
+    @Column(nullable = false)
+    private boolean active;
+
+    @Builder
+    public Admin(String loginId, String loginPw, String name, String tel, String position, boolean active) {
+        this.loginId = loginId;
+        this.loginPw = loginPw;
+        this.name = name;
+        this.tel = tel;
+        this.position = position;
+        this.active = active;
+    }
 }
