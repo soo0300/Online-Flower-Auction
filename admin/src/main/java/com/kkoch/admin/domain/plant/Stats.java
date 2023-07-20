f2fc6bdb89596f3a8838575a9dc93d108b9e10ca
임우택 <lyt1228@naver.com>
2023-07-20 11:09:57 +0900

    🎉feat: S09P12C204-72 Stats 도메인 설계

--- admin/src/main/java/com/kkoch/admin/domain/plant/Stats.java
+++ admin/src/main/java/com/kkoch/admin/domain/plant/Stats.java
@@ -1,4 +1,41 @@
 package com.kkoch.admin.domain.plant;
 
-public class Stats {
+import com.kkoch.admin.domain.Grade;
+import com.kkoch.admin.domain.TimeBaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Stats extends TimeBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "stats_id")
+    private Long id;
+
+    private int priceAvg;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, updatable = false, length = 20)
+    private Grade grade;
+
+    private int count;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "plant_id")
+    private Plant plant;
+
+    @Builder
+    public Stats(int priceAvg, Grade grade, int count, Plant plant) {
+        this.priceAvg = priceAvg;
+        this.grade = grade;
+        this.count = count;
+        this.plant = plant;
+    }
 }
