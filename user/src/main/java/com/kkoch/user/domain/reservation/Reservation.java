08045b3f92e96e48d6b9a5edf38eca16c5d3cb55
김수진 <soo0300@naver.com>
2023-07-20 12:08:23 +0900

    🎉feat: S09P12C204-118 거래예약 doamin 생성

--- user/src/main/java/com/kkoch/user/domain/reservation/Reservation.java
+++ user/src/main/java/com/kkoch/user/domain/reservation/Reservation.java
@@ -1,4 +1,28 @@
 package com.kkoch.user.domain.reservation;
 
-public class Reservation {
+import com.kkoch.user.domain.TimeBaseEntity;
+import com.kkoch.user.domain.member.Member;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "reservation")
+@Getter
+
+public class Reservation extends TimeBaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="reservation_id")
+    private Long id;
+    private Long memberId;
+    private int count;
+    private int price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private long plantId;
+
 }
