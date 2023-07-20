66f323b4ebf2f4e7fff21577f7148124a30cab5d
임우택 <lyt1228@naver.com>
2023-07-20 10:47:07 +0900

    🎉feat: S09P12C204-72 Trade 도메인 설계

--- user/src/main/java/com/kkoch/user/domain/trade/Trade.java
+++ user/src/main/java/com/kkoch/user/domain/trade/Trade.java
@@ -1,4 +1,40 @@
 package com.kkoch.user.domain.trade;
 
-public class Trade {
+import com.kkoch.user.domain.TimeBaseEntity;
+import com.kkoch.user.domain.member.Member;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+import static javax.persistence.FetchType.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Trade extends TimeBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "trade_id")
+    private Long id;
+
+    private int totalPrice;
+    private LocalDateTime tradeDate;
+    private boolean active;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Builder
+    private Trade(int totalPrice, LocalDateTime tradeDate, boolean active, Member member) {
+        this.totalPrice = totalPrice;
+        this.tradeDate = tradeDate;
+        this.active = active;
+        this.member = member;
+    }
 }
