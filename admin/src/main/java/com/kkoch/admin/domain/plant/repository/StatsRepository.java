81fa5813965249d29a1524699419a8bf72059dc9
임우택 <lyt1228@naver.com>
2023-07-20 11:36:27 +0900

    🌈style: S09P12C204-72 StatsRepository 패키지 이동

--- admin/src/main/java/com/kkoch/admin/domain/plant/repository/StatsRepository.java
+++ admin/src/main/java/com/kkoch/admin/domain/plant/repository/StatsRepository.java
@@ -1,5 +1,6 @@
-package com.kkoch.admin.domain.plant;
+package com.kkoch.admin.domain.plant.repository;
 
+import com.kkoch.admin.domain.plant.Stats;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
