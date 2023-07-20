7f37fb6b13d6a7c561512ea53bd08d14e9307f76
김수진 <soo0300@naver.com>
2023-07-20 13:26:37 +0900

    🎉feat: S09P12C204-118 거래예약 doamin 생성

--- user/src/main/java/com/kkoch/user/domain/reservation/Reservation.java
+++ user/src/main/java/com/kkoch/user/domain/reservation/Reservation.java
@@ -2,27 +2,38 @@ package com.kkoch.user.domain.reservation;
 
 import com.kkoch.user.domain.TimeBaseEntity;
 import com.kkoch.user.domain.member.Member;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "reservation")
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 
 public class Reservation extends TimeBaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="reservation_id")
+    @Column(name = "member_id", updatable = false, nullable = false)
     private Long id;
-    private Long memberId;
+
     private int count;
     private int price;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
+    @JoinColumn(name = "member_id", updatable = false, nullable = false)
     private Member member;
 
-    private long plantId;
+    private Long plantId;
 
+    @Builder
+    private Reservation(int count, int price, Member memeber, Long plantId) {
+        this.count = count;
+        this.price = price;
+        this.member = member;
+        this.plantId = plantId;
+    }
 }
