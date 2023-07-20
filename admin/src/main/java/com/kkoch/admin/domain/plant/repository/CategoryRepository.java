6658e4515c5ac102b98081a0167d07ee647b095f
홍승준 <ghdgn22@gmail.com>
2023-07-20 13:53:40 +0900

    👀code review: S09P12C204-88 회원, 알림, 식물, 카테고리 entity 수정

--- admin/src/main/java/com/kkoch/admin/domain/plant/repository/CategoryRepository.java
+++ admin/src/main/java/com/kkoch/admin/domain/plant/repository/CategoryRepository.java
@@ -1,10 +1,10 @@
 package com.kkoch.admin.domain.plant.repository;
 
 import com.kkoch.admin.domain.plant.Category;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CategoryRepository extends CrudRepository<Category, String> {
+public interface CategoryRepository extends JpaRepository<Category, String> {
 
 }
