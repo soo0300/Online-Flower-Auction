16ec8c404593321686e1223c5f95436ba5b0451b
임우택 <lyt1228@naver.com>
2023-07-20 11:00:10 +0900

    🎉feat: S09P12C204-72 AdminRepository 생성 및 DataJPA 적용

---
+++ admin/src/main/java/com/kkoch/admin/domain/admin/repository/AdminRepository.java
@@ -0,0 +1,9 @@
+package com.kkoch.admin.domain.admin.repository;
+
+import com.kkoch.admin.domain.admin.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {
+}
