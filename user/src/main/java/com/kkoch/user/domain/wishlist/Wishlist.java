bebbc4f34da5ebd94f31bef578edf36889582d03
leeyr0412 <leeyr0412@naver.com>
2023-07-20 10:53:28 +0900

    🎉feat: S09P12C204-108 Wishlist Entity 생성

--- user/src/main/java/com/kkoch/user/domain/wishlist/Wishlist.java
+++ user/src/main/java/com/kkoch/user/domain/wishlist/Wishlist.java
@@ -1,4 +1,36 @@
 package com.kkoch.user.domain.wishlist;
 
-public class Wishlist {
+import com.kkoch.user.domain.TimeBaseEntity;
+import com.kkoch.user.domain.member.Member;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = PROTECTED)
+public class Wishlist extends TimeBaseEntity {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "wishlist_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private Long auctionArticleId;
+
+    @Builder
+    public Wishlist(Long id, Member member, Long auctionArticleId) {
+        this.id = id;
+        this.member = member;
+        this.auctionArticleId = auctionArticleId;
+    }
 }
