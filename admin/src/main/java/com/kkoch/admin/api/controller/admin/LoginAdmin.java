d05baf195ceee38c54f5e4b0bb94426f0e9d31b2
leeyr0412 <leeyr0412@naver.com>
2023-07-21 17:25:23 +0900

    🎉feat: S09P12C204-99 로그인 한 관리자 Response 생성

---
+++ admin/src/main/java/com/kkoch/admin/api/controller/admin/LoginAdmin.java
@@ -0,0 +1,12 @@
+package com.kkoch.admin.api.controller.admin;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class LoginAdmin {
+
+    private Long id;
+    private String authority;
+}
