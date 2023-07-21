925ca6875193d7e838dc0db0b29ffd26197c61fe
임우택 <lyt1228@naver.com>
2023-07-21 15:49:50 +0900

    🔨test: S09P12C204-68 낙찰 내역 등록 컨트롤러 테스트 작성

--- admin/src/test/java/com/kkoch/admin/ControllerTestSupport.java
+++ admin/src/test/java/com/kkoch/admin/ControllerTestSupport.java
@@ -1,11 +1,12 @@
 package com.kkoch.admin;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kkoch.admin.api.controller.trade.TradeController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(controllers = {})
+@WebMvcTest(controllers = {TradeController.class})
 public abstract class ControllerTestSupport {
 
     @Autowired
