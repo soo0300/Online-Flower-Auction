cdfe6b3467cd35c9594b2eeba6e76fe50e94f2ed
chaos0103 <lyt1228@naver.com>
2023-07-22 17:02:57 +0900

    🔨test: S09P12C204-70 낙찰 내역 조회 컨트롤러 테스트 작성

--- admin/src/test/java/com/kkoch/admin/api/controller/trade/TradeControllerTest.java
+++ admin/src/test/java/com/kkoch/admin/api/controller/trade/TradeControllerTest.java
@@ -1,25 +1,35 @@
 package com.kkoch.admin.api.controller.trade;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.kkoch.admin.ControllerTestSupport;
 import com.kkoch.admin.api.controller.trade.request.AddTradeRequest;
 import com.kkoch.admin.api.controller.trade.request.AuctionArticleRequest;
+import com.kkoch.admin.api.controller.trade.response.TradeResponse;
+import com.kkoch.admin.api.service.trade.TradeQueryService;
 import com.kkoch.admin.api.service.trade.TradeService;
+import com.kkoch.admin.domain.trade.repository.dto.TradeSearchCond;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import javax.ws.rs.core.MediaType;
 import java.time.LocalDateTime;
 import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 class TradeControllerTest extends ControllerTestSupport {
 
     @MockBean
     private TradeService tradeService;
+
+    @MockBean
+    private TradeQueryService tradeQueryService;
 
     @DisplayName("경매를 완료하면 거래 내역이 등록 된다.")
     @Test
@@ -37,16 +47,40 @@ class TradeControllerTest extends ControllerTestSupport {
 
         //when //then
         mockMvc.perform(
-                        MockMvcRequestBuilders.post("/user-service/trades")
+                        post("/admin-service/trades")
                                 .content(objectMapper.writeValueAsString(request))
                                 .contentType(MediaType.APPLICATION_JSON)
                 )
-                .andDo(MockMvcResultHandlers.print())
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("200"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value("OK"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("SUCCESS"))
-                .andExpect(MockMvcResultMatchers.jsonPath("$.data").isNumber());
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.message").value("SUCCESS"))
+                .andExpect(jsonPath("$.data").isNumber());
+    }
+
+    @DisplayName("회원은 조회 기간과 페이지를 선택하여 본인의 거래 내역을 확인할 수 있다.")
+    @Test
+    void getMyTrades() throws Exception {
+        //given
+        List<TradeResponse> responses = List.of();
+        PageImpl<TradeResponse> tradeResponses = new PageImpl<>(responses);
+
+        BDDMockito.given(tradeQueryService.getMyTrades(anyLong(), any(TradeSearchCond.class), any(Pageable.class)))
+                .willReturn(tradeResponses);
+
+        //when //then
+        mockMvc.perform(
+                        get("/admin-service/trades/{memberId}", 1L)
+                                .queryParam("term", "1")
+                                .queryParam("page", "0")
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value("200"))
+                .andExpect(jsonPath("$.status").value("OK"))
+                .andExpect(jsonPath("$.message").value("SUCCESS"))
+                .andExpect(jsonPath("$.data.content").isArray());
     }
 
     private AuctionArticleRequest createAuctionArticleRequest(Long auctionArticleId, int bidPrice) {
