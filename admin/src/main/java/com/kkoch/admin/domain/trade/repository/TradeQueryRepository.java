56c4f31d1df5f35ccb1b537f62c93e5fb7e15430
chaos0103 <lyt1228@naver.com>
2023-07-22 16:13:34 +0900

    ♻️refactor: S09P12C204-70 낙찰 내역 조회 쿼리 프로젝션 적용

--- admin/src/main/java/com/kkoch/admin/domain/trade/repository/TradeQueryRepository.java
+++ admin/src/main/java/com/kkoch/admin/domain/trade/repository/TradeQueryRepository.java
@@ -1,7 +1,8 @@
 package com.kkoch.admin.domain.trade.repository;
 
-import com.kkoch.admin.domain.trade.Trade;
+import com.kkoch.admin.api.controller.trade.response.TradeResponse;
 import com.kkoch.admin.domain.trade.repository.dto.TradeSearchCond;
+import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
@@ -23,7 +24,7 @@ public class TradeQueryRepository {
         this.queryFactory = new JPAQueryFactory(em);
     }
 
-    public List<Trade> findByCondition(Long memberId, TradeSearchCond cond, Pageable pageable) {
+    public List<TradeResponse> findByCondition(Long memberId, TradeSearchCond cond, Pageable pageable) {
         List<Long> ids = queryFactory
                 .select(trade.id)
                 .from(trade)
@@ -42,7 +43,13 @@ public class TradeQueryRepository {
         }
 
         return queryFactory
-                .select(trade)
+                .select(Projections.constructor(TradeResponse.class,
+                        trade.id,
+                        trade.totalPrice,
+                        trade.tradeDate,
+                        trade.pickupStatus,
+                        trade.articles.size()
+                ))
                 .from(trade)
                 .where(trade.id.in(ids))
                 .orderBy(trade.createdDate.desc())
