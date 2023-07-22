bed590476741d9f687f8cc1ede855417fb38f826
chaos0103 <lyt1228@naver.com>
2023-07-22 13:11:40 +0900

    🎉feat: S09P12C204-70 낙찰 내역 조회 쿼리 구현

---
+++ admin/src/main/java/com/kkoch/admin/domain/trade/repository/TradeQueryRepository.java
@@ -0,0 +1,51 @@
+package com.kkoch.admin.domain.trade.repository;
+
+import com.kkoch.admin.domain.trade.Trade;
+import com.kkoch.admin.domain.trade.repository.dto.TradeSearchCond;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.CollectionUtils;
+
+import javax.persistence.EntityManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.kkoch.admin.domain.trade.QTrade.*;
+
+@Repository
+public class TradeQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    public TradeQueryRepository(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    public List<Trade> findByCondition(Long memberId, TradeSearchCond cond, Pageable pageable) {
+        List<Long> ids = queryFactory
+                .select(trade.id)
+                .from(trade)
+                .where(
+                        trade.memberId.eq(memberId),
+                        trade.active.isTrue(),
+                        trade.tradeDate.between(cond.getStartDateTime(), cond.getEndDateTime())
+                )
+                .orderBy(trade.createdDate.desc())
+                .limit(pageable.getPageSize())
+                .offset(pageable.getOffset())
+                .fetch();
+
+        if (CollectionUtils.isEmpty(ids)) {
+            return new ArrayList<>();
+        }
+
+        return queryFactory
+                .select(trade)
+                .from(trade)
+                .where(trade.id.in(ids))
+                .orderBy(trade.createdDate.desc())
+                .fetch();
+    }
+}
