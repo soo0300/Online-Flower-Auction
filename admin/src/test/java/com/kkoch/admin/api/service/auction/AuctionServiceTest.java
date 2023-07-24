bdc1b546e3194b5c11a3535ce94b8626cc605c29
leeyr0412 <leeyr0412@naver.com>
2023-07-24 17:52:18 +0900

    🔨test: S09P12C204-81 경매 일정 상태변경 테스트 작성

--- admin/src/test/java/com/kkoch/admin/api/service/auction/AuctionServiceTest.java
+++ admin/src/test/java/com/kkoch/admin/api/service/auction/AuctionServiceTest.java
@@ -4,6 +4,7 @@ package com.kkoch.admin.api.service.auction;
 import com.kkoch.admin.IntegrationTestSupport;
 import com.kkoch.admin.api.controller.auction.response.AuctionTitleResponse;
 import com.kkoch.admin.api.service.auction.dto.AddAuctionDto;
+import com.kkoch.admin.api.service.auction.dto.SetAuctionStatusDto;
 import com.kkoch.admin.domain.admin.Admin;
 import com.kkoch.admin.domain.admin.repository.AdminRepository;
 import com.kkoch.admin.domain.auction.Auction;
@@ -15,7 +16,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.NoSuchElementException;
 import java.util.Optional;
+
+import static com.kkoch.admin.domain.auction.Status.*;
 
 
 @Transactional
@@ -28,7 +32,42 @@ class AuctionServiceTest extends IntegrationTestSupport {
     @Autowired
     private AdminRepository adminRepository;
 
-    @DisplayName("경매일정 등록시 구분코드 에러를 검증한다.")
+    @DisplayName("[경매 상태 수정] 경매상태를 수정할 때 잘못된 pk의 경매일정을 수정하면 에러가 발생한다.")
+    @Test
+    void setAuctionPKError() {
+        //given
+        SetAuctionStatusDto dto = SetAuctionStatusDto.builder()
+                .auctionId(2L)
+                .status(OPEN)
+                .build();
+
+        //when //then
+        Assertions.assertThatThrownBy(() -> auctionService.setStatus(dto))
+                .isInstanceOf(NoSuchElementException.class)
+                .hasMessage("잘못된 옥션 PK");
+
+    }
+
+    @DisplayName("[경매 상태 수정]")
+    @Test
+    void setAuction() {
+        //given
+        Admin admin = insertAdmin();
+
+        Auction savedAuction = insertAuction(admin);
+        SetAuctionStatusDto dto = SetAuctionStatusDto.builder()
+                .auctionId(savedAuction.getId())
+                .status(CLOSE)
+                .build();
+
+        //when //then
+        AuctionTitleResponse response = auctionService.setStatus(dto);
+        Assertions.assertThat(response.getTitle()).isEqualTo("절화 마감");
+
+    }
+
+
+    @DisplayName("[경매일정 등록]시 구분코드 에러를 검증한다.")
     @Test
     void addAuctionCodeError() {
         //given
@@ -46,7 +85,7 @@ class AuctionServiceTest extends IntegrationTestSupport {
                 .hasMessage("구분코드 에러");
     }
 
-    @DisplayName("경매일정 등록")
+    @DisplayName("[경매일정 등록]")
     @Test
     void addAuction() {
         //given
@@ -62,6 +101,18 @@ class AuctionServiceTest extends IntegrationTestSupport {
         //then
         Optional<Auction> findAuction = auctionRepository.findById(response.getAuctionId());
         Assertions.assertThat(findAuction).isPresent();
+    }
+
+
+    private Auction insertAuction(Admin admin) {
+        Auction auction = Auction.builder()
+                .code(1)
+                .startTime(LocalDateTime.of(2023, 9, 20, 5, 0))
+                .active(true)
+                .status(READY)
+                .admin(admin)
+                .build();
+        return auctionRepository.save(auction);
     }
 
     private Admin insertAdmin() {
