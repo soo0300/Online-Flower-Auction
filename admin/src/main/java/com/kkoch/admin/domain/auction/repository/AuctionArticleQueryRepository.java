7aaa24ad6c74c2c7e57c5560d9c1d6d3a1689b6c
임우택 <lyt1228@naver.com>
2023-07-24 14:06:05 +0900

    🎉feat: S09P12C204-154 낙찰 내역 상세 조회 쿼리 구현

---
+++ admin/src/main/java/com/kkoch/admin/domain/auction/repository/AuctionArticleQueryRepository.java
@@ -0,0 +1,47 @@
+package com.kkoch.admin.domain.auction.repository;
+
+import com.kkoch.admin.api.controller.trade.response.AuctionArticleResponse;
+import com.kkoch.admin.domain.plant.QCategory;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static com.kkoch.admin.domain.auction.QAuctionArticle.*;
+import static com.kkoch.admin.domain.plant.QPlant.*;
+
+@Repository
+public class AuctionArticleQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    public AuctionArticleQueryRepository(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    public List<AuctionArticleResponse> findByTradeId(Long tradeId) {
+        QCategory code = new QCategory("code");
+        QCategory name = new QCategory("name");
+        QCategory type = new QCategory("type");
+        return queryFactory
+                .select(Projections.constructor(AuctionArticleResponse.class,
+                        auctionArticle.plant.code.name,
+                        auctionArticle.plant.name.name,
+                        auctionArticle.plant.type.name,
+                        auctionArticle.grade,
+                        auctionArticle.count,
+                        auctionArticle.bidPrice,
+                        auctionArticle.bidTime,
+                        auctionArticle.region
+                        ))
+                .from(auctionArticle)
+                .join(auctionArticle.plant, plant)
+                .join(plant.code, code)
+                .join(plant.name, name)
+                .join(plant.type, type)
+                .where(auctionArticle.trade.id.eq(tradeId))
+                .fetch();
+    }
+}
