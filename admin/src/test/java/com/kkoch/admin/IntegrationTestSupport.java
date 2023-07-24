3b42c30b8f65b0204921cf08383ef2c73711af93
임우택 <lyt1228@naver.com>
2023-07-24 16:15:38 +0900

    🎉feat: S09P12C204-99 충돌 해결

--- admin/src/test/java/com/kkoch/admin/IntegrationTestSupport.java
+++
@@ -1,9 +0,0 @@
-package com.kkoch.admin;
-
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-
-@ActiveProfiles("test")
-@SpringBootTest
-public abstract class IntegrationTestSupport {
-}
