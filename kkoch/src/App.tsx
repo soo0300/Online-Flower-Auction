fc5fd739d12f398b5f7ca4c3f13c897a5090abf9
신성주 <hans52410537@gmail.com>
2023-07-24 17:29:33 +0900

    🎉 feat: S09P12C204-131 네비게이션 바 구현

--- kkoch/src/App.tsx
+++ kkoch/src/App.tsx
@@ -1,14 +1,35 @@
 import Navbar from "@/scenes/navbar";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { SelectedPage } from "./shared/types";
 
 
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [isTop, setIsTop] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.scrollY === 0) {
+        setIsTop(true);
+        setSelectedPage(SelectedPage.Home);
+      } 
+
+      if(window.scrollY !== 0) {
+        setIsTop(false);
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <div className="app bg-gray-20">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar 
+        isTop = {isTop}  
+        selectedPage={selectedPage} setSelectedPage={setSelectedPage} 
+      />
     </div>
   )
 }
--- kkoch/src/scenes/navbar/index.tsx
+++ kkoch/src/scenes/navbar/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"; 
-import { Bars3Icon } from "@heroicons/react/24/solid";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/logo.png";
 import Link from './Link';
 import { SelectedPage } from '@/shared/types';
@@ -8,21 +8,22 @@ import ActionButton from "@/shared/ActionButton";
 
 
 type Props = {
+  isTop: boolean;
   selectedPage: SelectedPage;
   setSelectedPage: (value: SelectedPage) => void;
 };
 
-const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
+const Navbar = ({ isTop, selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-between";
   
   // 모바일 용 토글버튼은 false로 초기화 
   const [isMenuToggled, menuToggle] = useState<boolean>(false);
-  
   const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)");
+  const navbarBackground = isTop ? "" : "bg-primary-100 drop-shadow";
 
   return <nav>
     <div
-      className={`${flexBetween} fixed top-0 z-30 w-full py-6 border-b-2`}
+      className={`${navbarBackground} ${flexBetween} fixed top-0 z-30 w-full py-6 border-b-2`}
     >
       <div className={`${flexBetween} mx-auto w-5/6`}>
         <div className={`${flexBetween} w-full gap-16`}>
@@ -53,9 +54,28 @@ const Navbar = ({ selectedPage, setSelectedPage }: Props) => {
             )}
         </div>
       </div>
-      
     </div>  
-  
+    
+    {/* 모바일 화면 부분 */}
+    {!isAboveMediumScreens && isMenuToggled && (
+      <div className="fixed right-0 bottom-0 z-40 h-full w-[300px] bg-primary-100 drop-shadow-xl">
+        
+        {/* 메뉴 닫기 */}
+        <div className="flex justify-end p-12 ">
+          <button onClick={() => menuToggle(!isMenuToggled)}>
+            <XMarkIcon className="h-6 w-6 text-gray-400" />
+          </button>
+        </div>
+
+        {/* 메뉴 목록 */}
+        <div className="ml-[33%] flex flex-col gap-10 text-2xl">
+          <Link page="메인페이지" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+          <Link page="경매" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+          <Link page="꽃 정보" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+          <Link page="고객센터" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+        </div>
+      </div>
+    )}
   </nav>
 }
 
