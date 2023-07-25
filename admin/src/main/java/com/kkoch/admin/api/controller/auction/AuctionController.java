922da9407d8dde00f185a4cb742663ba26163f2d
leeyr0412 <leeyr0412@naver.com>
2023-07-25 11:50:59 +0900

    🎉feat: S09P12C204-81 경매 일정 상태변경 컨트롤러 구현

--- admin/src/main/java/com/kkoch/admin/api/controller/auction/AuctionController.java
+++ admin/src/main/java/com/kkoch/admin/api/controller/auction/AuctionController.java
@@ -6,14 +6,14 @@ import com.kkoch.admin.api.controller.auction.request.AddAuctionRequest;
 import com.kkoch.admin.api.controller.auction.response.AuctionTitleResponse;
 import com.kkoch.admin.api.service.auction.AuctionService;
 import com.kkoch.admin.api.service.auction.dto.AddAuctionDto;
+import com.kkoch.admin.api.service.auction.dto.SetAuctionStatusDto;
+import com.kkoch.admin.domain.auction.Status;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.time.LocalDateTime;
-
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 @RequiredArgsConstructor
 @RestController
@@ -24,11 +24,12 @@ public class AuctionController {
     private final AuctionService auctionService;
 
     @PostMapping
-    public ApiResponse<AuctionTitleResponse> addAuction(@Valid @RequestBody AddAuctionRequest request,
-                                                        @SessionAttribute(name = "loginAdmin") LoginAdmin loginAdmin) {
+    public ApiResponse<AuctionTitleResponse> addAuction(
+            @Valid @RequestBody AddAuctionRequest request,
+            @SessionAttribute(name = "loginAdmin") LoginAdmin loginAdmin) {
         LocalDateTime startTime = request.getStartTime();
         if (!startTime.isAfter(LocalDateTime.now().plusHours(1))) {
-            return ApiResponse.of(BAD_REQUEST, "경매 시간 입력 오류", null);
+            throw new IllegalArgumentException("경매 시간 입력 오류");
         }
 
         AddAuctionDto dto = request.toAddAuctionDto();
@@ -38,8 +39,23 @@ public class AuctionController {
         return ApiResponse.ok(response);
     }
 
-    @PatchMapping
-    public ApiResponse<AuctionTitleResponse> setAuctionStatus() {
-        return null;
+    @PatchMapping("{auctionId}/{status}")
+    public ApiResponse<AuctionTitleResponse> setAuctionStatus(
+            @PathVariable Long auctionId,
+            @PathVariable Status status,
+            @SessionAttribute(name = "loginAdmin") LoginAdmin loginAdmin) {
+        SetAuctionStatusDto dto = toSetAuctionStatusDto(auctionId, status);
+
+        AuctionTitleResponse response = auctionService.setStatus(dto);
+
+        log.debug("[경매 일정 상태 변경] 경매방 제목 = {}", response.getTitle());
+        return ApiResponse.ok(response);
+    }
+
+    private static SetAuctionStatusDto toSetAuctionStatusDto(Long auctionId, Status status) {
+        return SetAuctionStatusDto.builder()
+                .status(status)
+                .auctionId(auctionId)
+                .build();
     }
 }
