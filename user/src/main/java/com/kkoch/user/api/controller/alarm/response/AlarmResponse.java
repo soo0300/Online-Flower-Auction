71b068cf320fabc411f811b7f6df07c0302430ab
임우택 <lyt1228@naver.com>
2023-07-25 15:29:47 +0900

    🔨test: S09P12C204-172 알림 조회 쿼리 테스트 작성

--- user/src/main/java/com/kkoch/user/api/controller/alarm/response/AlarmResponse.java
+++ user/src/main/java/com/kkoch/user/api/controller/alarm/response/AlarmResponse.java
@@ -2,10 +2,21 @@ package com.kkoch.user.api.controller.alarm.response;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @Data
 public class AlarmResponse {
-    private Long id;
+
+    private Long alarmId;
     private String content;
     private boolean open;
     private String createDate;
+
+    public AlarmResponse(Long alarmId, String content, boolean open, LocalDateTime createDate) {
+        this.alarmId = alarmId;
+        this.content = content;
+        this.open = open;
+        this.createDate = createDate.format(DateTimeFormatter.ofPattern("yyyy.MM.dd hh:mm"));
+    }
 }
