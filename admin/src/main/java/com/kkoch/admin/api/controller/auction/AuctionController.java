0bd540ca4edcdeeacb35bac02b16a8a429847aca
leeyr0412 <leeyr0412@naver.com>
2023-07-25 14:48:41 +0900

    🎉feat: S09P12C204-81 경매 일정 변경 컨트롤러 구현

--- admin/src/main/java/com/kkoch/admin/api/controller/auction/AuctionController.java
+++ admin/src/main/java/com/kkoch/admin/api/controller/auction/AuctionController.java
@@ -3,9 +3,11 @@ package com.kkoch.admin.api.controller.auction;
 import com.kkoch.admin.api.ApiResponse;
 import com.kkoch.admin.api.controller.admin.LoginAdmin;
 import com.kkoch.admin.api.controller.auction.request.AddAuctionRequest;
+import com.kkoch.admin.api.controller.auction.request.SetAuctionRequest;
 import com.kkoch.admin.api.controller.auction.response.AuctionTitleResponse;
 import com.kkoch.admin.api.service.auction.AuctionService;
 import com.kkoch.admin.api.service.auction.dto.AddAuctionDto;
+import com.kkoch.admin.api.service.auction.dto.SetAuctionDto;
 import com.kkoch.admin.api.service.auction.dto.SetAuctionStatusDto;
 import com.kkoch.admin.domain.auction.Status;
 import lombok.RequiredArgsConstructor;
@@ -27,10 +29,7 @@ public class AuctionController {
     public ApiResponse<AuctionTitleResponse> addAuction(
             @Valid @RequestBody AddAuctionRequest request,
             @SessionAttribute(name = "loginAdmin") LoginAdmin loginAdmin) {
-        LocalDateTime startTime = request.getStartTime();
-        if (!startTime.isAfter(LocalDateTime.now().plusHours(1))) {
-            throw new IllegalArgumentException("경매 시간 입력 오류");
-        }
+        validationTime(request.getStartTime());
 
         AddAuctionDto dto = request.toAddAuctionDto();
 
@@ -39,7 +38,7 @@ public class AuctionController {
         return ApiResponse.ok(response);
     }
 
-    @PatchMapping("{auctionId}/{status}")
+    @PatchMapping("/{auctionId}/{status}")
     public ApiResponse<AuctionTitleResponse> setAuctionStatus(
             @PathVariable Long auctionId,
             @PathVariable Status status,
@@ -50,6 +49,32 @@ public class AuctionController {
 
         log.debug("[경매 일정 상태 변경] 경매방 제목 = {}", response.getTitle());
         return ApiResponse.ok(response);
+    }
+
+    @PatchMapping("/{auctionId}")
+    public ApiResponse<AuctionTitleResponse> setAuction(
+            @PathVariable Long auctionId,
+            @Valid @RequestBody SetAuctionRequest request,
+            @SessionAttribute(name = "loginAdmin") LoginAdmin loginAdmin) {
+
+        LocalDateTime startTime = validationTime(request.getStartTime());
+
+        SetAuctionDto dto = SetAuctionDto.builder()
+                .auctionId(auctionId)
+                .code(request.getCode())
+                .startTime(startTime)
+                .build();
+
+        AuctionTitleResponse response = auctionService.setAuction(loginAdmin.getId(), dto);
+        return ApiResponse.ok(response);
+    }
+
+    private static LocalDateTime validationTime(LocalDateTime request) {
+        LocalDateTime startTime = request;
+        if (!startTime.isAfter(LocalDateTime.now().plusHours(1))) {
+            throw new IllegalArgumentException("경매 시간 입력 오류");
+        }
+        return startTime;
     }
 
     private static SetAuctionStatusDto toSetAuctionStatusDto(Long auctionId, Status status) {
