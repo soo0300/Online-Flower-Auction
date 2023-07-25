b90964f88a6ba07e47f739d49cf8a02520caf305
leeyr0412 <leeyr0412@naver.com>
2023-07-25 11:50:35 +0900

    🔨test: S09P12C204-81 경매 일정 상태변경 컨트롤러 테스트 구현

--- admin/src/main/java/com/kkoch/admin/api/controller/admin/LoginAdmin.java
+++ admin/src/main/java/com/kkoch/admin/api/controller/admin/LoginAdmin.java
@@ -1,5 +1,6 @@
 package com.kkoch.admin.api.controller.admin;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,4 +10,10 @@ public class LoginAdmin {
 
     private Long id;
     private String authority;
+
+    @Builder
+    public LoginAdmin(Long id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
 }
--- admin/src/test/java/com/kkoch/admin/api/controller/auction/AuctionControllerTest.java
+++ admin/src/test/java/com/kkoch/admin/api/controller/auction/AuctionControllerTest.java
@@ -6,9 +6,13 @@ import com.kkoch.admin.api.controller.auction.request.AddAuctionRequest;
 import com.kkoch.admin.api.controller.auction.response.AuctionTitleResponse;
 import com.kkoch.admin.api.service.auction.AuctionService;
 import com.kkoch.admin.api.service.auction.dto.AddAuctionDto;
+import com.kkoch.admin.api.service.auction.dto.SetAuctionStatusDto;
+import com.kkoch.admin.domain.admin.Admin;
+import com.kkoch.admin.domain.auction.Auction;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
@@ -18,17 +22,47 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDateTime;
 
+import static com.kkoch.admin.domain.auction.Status.CLOSE;
+import static com.kkoch.admin.domain.auction.Status.OPEN;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 
-
+@WebMvcTest(controllers = {AuctionController.class})
 class AuctionControllerTest extends ControllerTestSupport {
 
     @MockBean
     private AuctionService auctionService;
 
+    @DisplayName("[경매 상태 수정]")
     @Test
-    @DisplayName("경매일정 등록 시 현재시간부터 1시간 이내에 등록하면 에러가 발생한다.")
+    void setStatusError() throws Exception {
+        //given
+        MockHttpSession session = getLoginAdminSession();
+
+        SetAuctionStatusDto dto = SetAuctionStatusDto.builder()
+                .auctionId(3L)
+                .status(CLOSE)
+                .build();
+        AuctionTitleResponse response = AuctionTitleResponse.builder()
+                .auctionId(3L)
+                .title("title")
+                .build();
+        BDDMockito.given(auctionService.setStatus(dto))
+                .willReturn(response);
+
+        Long auctionId = 3L;
+        //when //then
+        mockMvc.perform(
+                        MockMvcRequestBuilders.patch("/admin-service/auctions/{auctionId}/{status}", auctionId, CLOSE)
+                                .session(session)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.title").value("title"));
+    }
+
+    @Test
+    @DisplayName("[경매일정 등록] 시 현재시간부터 1시간 이내에 등록하면 에러가 발생한다.")
     void addAuctionTimeException() throws Exception {
         //given
         AddAuctionRequest request = new AddAuctionRequest(LocalDateTime.now().plusMinutes(30), 1);
@@ -45,11 +79,11 @@ class AuctionControllerTest extends ControllerTestSupport {
                                 .session(session)
                 )
                 .andDo(MockMvcResultHandlers.print())
-                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value(400));
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
     }
 
     @Test
-    @DisplayName("경매일정 등록 성공")
+    @DisplayName("[경매일정 등록] 성공")
     void addAuction() throws Exception {
         //given
         AuctionTitleResponse response = AuctionTitleResponse.builder()
@@ -58,14 +92,12 @@ class AuctionControllerTest extends ControllerTestSupport {
                 .build();
         AddAuctionRequest request = new AddAuctionRequest(LocalDateTime.now().plusHours(2), 1);
 
-        MockHttpSession session = new MockHttpSession();
-        session.setAttribute("loginAdmin", new LoginAdmin());
+        MockHttpSession session = getLoginAdminSession();
 
         BDDMockito.given(auctionService.addAuction(anyLong(), any(AddAuctionDto.class)))
                 .willReturn(response);
 
         //when //then
-        // TODO: 2023-07-24 null 들어감 
         mockMvc.perform(
                         MockMvcRequestBuilders.post("/admin-service/auctions")
                                 .content(objectMapper.writeValueAsString(request))
@@ -75,5 +107,15 @@ class AuctionControllerTest extends ControllerTestSupport {
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.title").value("title"));
 
+    }
+
+    private static MockHttpSession getLoginAdminSession() {
+        MockHttpSession session = new MockHttpSession();
+        LoginAdmin loginAdmin = LoginAdmin.builder()
+                .id(1L)
+                .authority("10")
+                .build();
+        session.setAttribute("loginAdmin", loginAdmin);
+        return session;
     }
 }
