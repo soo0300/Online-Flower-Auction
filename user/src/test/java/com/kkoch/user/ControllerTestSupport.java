c86186cef8799b9fecc925e6a0450eb153410c30
임우택 <lyt1228@naver.com>
2023-07-26 11:01:30 +0900

    🔨test: S09P12C204-172 알림 조회 컨트롤러 테스트 작성

--- user/src/test/java/com/kkoch/user/ControllerTestSupport.java
+++ user/src/test/java/com/kkoch/user/ControllerTestSupport.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest(controllers = {MemberController.class})
 public abstract class ControllerTestSupport {
 
     @Autowired
