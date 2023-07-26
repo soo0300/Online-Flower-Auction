47d660b0fd8e8a7bfa33dbe39bbd05ad38365a5a
임우택 <lyt1228@naver.com>
2023-07-26 10:33:07 +0900

    🎉feat: S09P12C204-172 이메일로 회원 PK 조회 쿼리 구현

--- user/src/main/java/com/kkoch/user/domain/member/repository/MemberRepository.java
+++ user/src/main/java/com/kkoch/user/domain/member/repository/MemberRepository.java
@@ -2,6 +2,8 @@ package com.kkoch.user.domain.member.repository;
 
 import com.kkoch.user.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,4 +11,7 @@ import java.util.Optional;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
+
+    @Query("select m.id from Member m where m.email =:email")
+    Optional<Long> findIdByEmail(@Param("email") String email);
 }
