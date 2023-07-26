aca816abeac0cfa5820ad9266f2f7988c46c100d
dong82 <lyt1228@naver.com>
2023-07-27 07:45:54 +0900

    ♻️refactor: S09P12C204-183 edit interceptor range

--- admin/src/main/java/com/kkoch/admin/config/WebConfig.java
+++ admin/src/main/java/com/kkoch/admin/config/WebConfig.java
@@ -22,7 +22,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new LoginCheckInterceptor())
             .order(1)
             .addPathPatterns("/**")
-            .excludePathPatterns("/", "/login/**", "/sign-up/**",
+            .excludePathPatterns("/**", "/login/**", "/sign-up/**",
                 "/css/**", "/file/**", "/icon/**", "/img/**", "/js/**");
     }
 }
