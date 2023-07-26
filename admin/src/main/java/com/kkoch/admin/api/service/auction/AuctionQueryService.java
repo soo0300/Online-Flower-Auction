49b9ffe065ab59a013431da0528e4120a58701db
이예리 <leeyr0412@naver.com>
2023-07-26 11:23:37 +0900

    🎉feat: S09P12C204-91 경매 일정 목록 조회 Service 구현

--- admin/src/main/java/com/kkoch/admin/api/service/auction/AuctionQueryService.java
+++ admin/src/main/java/com/kkoch/admin/api/service/auction/AuctionQueryService.java
@@ -1,19 +1,25 @@
 package com.kkoch.admin.api.service.auction;
 
+import com.kkoch.admin.api.controller.auction.response.AuctionResponse;
 import com.kkoch.admin.api.service.auction.dto.AddAuctionDto;
+import com.kkoch.admin.domain.auction.repository.AuctionQueryRepository;
 import com.kkoch.admin.domain.auction.repository.AuctionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @Service
-@Transactional
+@Transactional(readOnly = true)
 public class AuctionQueryService {
 
-    private final AuctionRepository auctionRepository;
+    private final AuctionQueryRepository auctionQueryRepository;
 
-    public Long addAuction(AddAuctionDto dto) {
-        return null;
+    public List<AuctionResponse> getAuctionSchedule() {
+        return auctionQueryRepository.findAllAuction();
     }
 }
