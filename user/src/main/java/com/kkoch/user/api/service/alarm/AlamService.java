74341d5712f5efe99bfc27f0e0c18669f2b02690
임우택 <lyt1228@naver.com>
2023-07-26 11:52:58 +0900

    🎉feat: S09P12C204-170 알림 열람 서비스 구현

--- user/src/main/java/com/kkoch/user/api/service/alarm/AlamService.java
+++ user/src/main/java/com/kkoch/user/api/service/alarm/AlamService.java
@@ -1,6 +1,6 @@
 package com.kkoch.user.api.service.alarm;
 
-import com.kkoch.user.domain.alarm.repository.AlarmRepository;
+import com.kkoch.user.domain.alarm.repository.AlarmCommandRepository;
 import com.kkoch.user.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,10 +11,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class AlamService {
 
-    private final AlarmRepository alarmRepository;
+    private final AlarmCommandRepository alarmCommandRepository;
     private final MemberRepository memberRepository;
 
     public int open(String email) {
-        return 0;
+        Long memberId = memberRepository.findIdByEmail(email)
+            .orElseThrow();
+
+        return alarmCommandRepository.updateOpen(memberId);
     }
 }
