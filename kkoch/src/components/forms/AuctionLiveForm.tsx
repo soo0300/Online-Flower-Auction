95a64b51a06905dd56123a1d95fd89cdcea02d8d
서용준 <seoyj505@naver.com>
2023-07-27 12:10:37 +0900

    🎉feat: S09P12C204-258 경매방 화훼정보 나가기, 위시리스트버튼 구현, CSS

--- kkoch/src/components/forms/AuctionLiveForm.tsx
+++ kkoch/src/components/forms/AuctionLiveForm.tsx
@@ -18,8 +18,26 @@ const AuctionLiveForm = () => {
 
   return (
     <div className='gap-24 bg-gray-20 py-28 md:h-full md:pb-0'>
-      <div className='auction-heading'>
-        at화훼 양재공판장 경매방 경매중!!
+      <div className='auction-top'>
+        <button className='leave-button'>
+          <svg 
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="black"
+            className="w-10 h-10"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" 
+            />
+          </svg>
+        </button>
+        <div className='auction-heading'>
+          at화훼 양재공판장 경매방 경매중!!
+        </div>
       </div>
       <div className='auction-content'>
         <div className='stream-image'>
@@ -30,6 +48,23 @@ const AuctionLiveForm = () => {
             보유 포인트: { auctionInfo.point }
           </div>
           <div className='auction-border'>
+            <div className='favorite-icon'>
+              <button className='favorite-button'>
+                <svg 
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="yellow"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="white"
+                  className="w-6 h-6">
+                  <path 
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z"
+                  />
+                </svg>
+              </button>
+            </div>
             <div className='auction-status'>
               경매중
             </div>
