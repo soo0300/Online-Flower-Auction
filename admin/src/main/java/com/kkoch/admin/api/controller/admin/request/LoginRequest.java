7ab0339eaaa587127733a97fd2187b00190b2b1e
김수진 <soo0300@naver.com>
2023-07-27 16:10:37 +0900

    🎉feat: S09P12C204-244 관계자 로그인 컨트롤러 구현

---
+++ admin/src/main/java/com/kkoch/admin/api/controller/admin/request/LoginRequest.java
@@ -0,0 +1,32 @@
+package com.kkoch.admin.api.controller.admin.request;
+
+import com.kkoch.admin.api.service.admin.dto.LoginDto;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@RequiredArgsConstructor
+public class LoginRequest {
+
+    @NotBlank
+    private String loginId;
+    @NotBlank
+    private String loginPw;
+
+    @Builder
+    public LoginRequest(String loginId, String loginPw) {
+        this.loginId = loginId;
+        this.loginPw = loginPw;
+    }
+
+    public LoginDto toLoginRequestDto() {
+        LoginDto dto = LoginDto.builder()
+                .loginId(this.loginId)
+                .loginPw(this.loginPw)
+                .build();
+        return dto;
+    }
+}
