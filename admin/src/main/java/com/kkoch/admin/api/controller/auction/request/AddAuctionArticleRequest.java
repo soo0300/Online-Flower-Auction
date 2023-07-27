4b4457f4e40ed833d59fc477c3f4d3016ffa5726
이예리 <leeyr0412@naver.com>
2023-07-27 17:19:13 +0900

    🎉feat: S09P12C204-175 경매품 등록 Controller 구현

---
+++ admin/src/main/java/com/kkoch/admin/api/controller/auction/request/AddAuctionArticleRequest.java
@@ -0,0 +1,50 @@
+package com.kkoch.admin.api.controller.auction.request;
+
+import com.kkoch.admin.api.service.auction.dto.AddAuctionArticleDto;
+import com.kkoch.admin.domain.Grade;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+public class AddAuctionArticleRequest {
+
+    @NotNull
+    private Long plantId;
+    @NotNull
+    private Long auctionId;
+    @NotNull
+    private Grade grade;
+    @NotNull
+    private Integer count;
+    @NotNull
+    private String region;
+    @NotNull
+    private String shipper;
+    @NotNull
+    private Integer startPrice;
+
+    @Builder
+    private AddAuctionArticleRequest(Long plantId, Long auctionId, Grade grade, Integer count, String region, String shipper, Integer startPrice) {
+        this.plantId = plantId;
+        this.auctionId = auctionId;
+        this.grade = grade;
+        this.count = count;
+        this.region = region;
+        this.shipper = shipper;
+        this.startPrice = startPrice;
+    }
+
+    public AddAuctionArticleDto toAddAuctionArticleDto() {
+        return AddAuctionArticleDto.builder()
+                .grade(this.grade)
+                .count(this.count)
+                .region(this.region)
+                .shipper(this.shipper)
+                .startPrice(this.startPrice)
+                .build();
+    }
+}
