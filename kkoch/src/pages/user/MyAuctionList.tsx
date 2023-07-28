ff299392b04f7cd6c3be4624016690f752725ce3
신성주 <hans52410537@gmail.com>
2023-07-28 16:09:29 +0900

    💿backup: S09P12C204-264 마이페이지 구현중

--- kkoch/src/pages/user/MyAuctionList.tsx
+++ kkoch/src/pages/user/MyAuctionList.tsx
@@ -1,9 +1,24 @@
 import { MDBCard, MDBCardBody } from "mdb-react-ui-kit";
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+
+const rows: GridRowsProp = [
+  { id: 1, col1: 'Hello', col2: 'World' },
+  { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+  { id: 3, col1: 'MUI', col2: 'is Amazing' },
+];
+
+const columns: GridColDef[] = [
+  { field: 'col1', headerName: 'Column 1', width: 150 },
+  { field: 'col2', headerName: 'Column 2', width: 150 },
+];
 
 function MyAuctionList() {
+
   return (
     <MDBCard>
-      <MDBCardBody>This is some text within a card body.</MDBCardBody>
+      <MDBCardBody>
+        <DataGrid rows={rows} columns={columns} />
+      </MDBCardBody>
     </MDBCard>
   )
 }
--- kkoch/src/pages/user/MyProfile.tsx
+++ kkoch/src/pages/user/MyProfile.tsx
@@ -14,18 +14,16 @@ const MyProfile = () => {
       <MDBRow>
         <MDBCol lg="4">
           <MDBCard className="mb-4">
-            <MDBCardBody className="text-center">
+            <MDBCardBody className="text-center flex flex-col items-center justify-center">
               <MDBCardImage
                 src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp"
-                alt="avatar"
-                className="rounded-circle"
-                style={{ width: '150px' }}
+                alt="profile"
+                className="rounded-circle w-[150px] mb-3"
                 fluid />
               <p className="text-muted mb-1">Full Stack Developer</p>
               <p className="text-muted mb-4">Bay Area, San Francisco, CA</p>
-              <div className="d-flex justify-content-center mb-2">
-                <MDBBtn>Follow</MDBBtn>
-                <MDBBtn outline className="ms-1">Message</MDBBtn>
+              <div className="mb-2">
+                <MDBBtn>프로필 변경</MDBBtn>
               </div>
             </MDBCardBody>
           </MDBCard>
@@ -36,10 +34,10 @@ const MyProfile = () => {
             <MDBCardBody>
               <MDBRow>
                 <MDBCol sm="3">
-                  <MDBCardText>Full Name</MDBCardText>
+                  <MDBCardText>이름</MDBCardText>
                 </MDBCol>
                 <MDBCol sm="9">
-                  <MDBCardText className="text-muted">Johnatan Smith</MDBCardText>
+                  <MDBCardText className="text-muted">김싸피</MDBCardText>
                 </MDBCol>
               </MDBRow>
               <hr />
