7d98af066035e4ccb1445c8eb912a2e706ce4835
이예리 <leeyr0412@naver.com>
2023-07-31 15:15:54 +0900

    🎉feat: S09P12C204-176 회원용 실시간 경매 실적 목록 출력 Service 구현

---
+++ admin/src/main/java/com/kkoch/admin/api/service/auction/AuctionArticleQueryService.java
@@ -0,0 +1,28 @@
+package com.kkoch.admin.api.service.auction;
+
+import com.kkoch.admin.api.controller.auction.response.AuctionArticleForMemberResponse;
+import com.kkoch.admin.domain.auction.repository.AuctionArticleQueryRepository;
+import com.kkoch.admin.domain.auction.repository.dto.AuctionArticleSearchCond;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class AuctionArticleQueryService {
+
+    private final AuctionArticleQueryRepository auctionArticleQueryRepository;
+
+    public Page<AuctionArticleForMemberResponse> getAuctionArticleListForMember(AuctionArticleSearchCond cond, Pageable pageable) {
+        List<AuctionArticleForMemberResponse> responses = auctionArticleQueryRepository.getAuctionArticleListForMember(cond, pageable);
+        int totalCount = auctionArticleQueryRepository.getTotalCount(cond);
+        return new PageImpl<>(responses, pageable, totalCount);
+    }
+
+}
