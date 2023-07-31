78e3be89feafcbb81fdd1c9adf9b4f26b230c47f
신성주 <hans52410537@gmail.com>
2023-07-31 11:36:54 +0900

    🎉 feat: S09P12C204-264 마이페이지 구현

--- kkoch/src/components/buttons/ActionButton.tsx
+++ kkoch/src/components/buttons/ActionButton.tsx
@@ -1,16 +1,17 @@
-import AnchorLink from "react-anchor-link-smooth-scroll"
+import { Link } from "react-router-dom";
 
 type Props = {
-  children: React.ReactNode;
+  path: string;
+  str: string;
 }
 
-const ActionButton = ({ children }: Props) => {
+const ActionButton = ({ path, str }: Props) => {
   return (
-    <AnchorLink
+    <Link to={path}
       className="rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white"
     >
-      {children}
-    </AnchorLink>
+      {str}
+    </Link>
   )
 }
 
--- kkoch/src/pages/user/MyAuctionList.tsx
+++ kkoch/src/pages/user/MyAuctionList.tsx
@@ -1,23 +1,81 @@
-import { MDBCard, MDBCardBody } from "mdb-react-ui-kit";
-import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import { MDBCard, MDBCardBody, MDBCardTitle } from "mdb-react-ui-kit";
+import { DataGrid, GridRowsProp, GridColDef, gridClasses } from '@mui/x-data-grid';
+import { alpha, styled } from '@mui/material/styles';
+
+const ODD_OPACITY = 0.2;
+
+const StripedDataGrid = styled(DataGrid)(({ theme }) => ({
+  [`& .${gridClasses.row}.even`]: {
+    backgroundColor: theme.palette.grey[200],
+    '&:hover, &.Mui-hovered': {
+      backgroundColor: alpha(theme.palette.primary.main, ODD_OPACITY),
+      '@media (hover: none)': {
+        backgroundColor: 'transparent',
+      },
+    },
+    '&.Mui-selected': {
+      backgroundColor: alpha(
+        theme.palette.primary.main,
+        ODD_OPACITY + theme.palette.action.selectedOpacity,
+      ),
+      '&:hover, &.Mui-hovered': {
+        backgroundColor: alpha(
+          theme.palette.primary.main,
+          ODD_OPACITY +
+            theme.palette.action.selectedOpacity +
+            theme.palette.action.hoverOpacity,
+        ),
+        // Reset on touch devices, it doesn't add specificity
+        '@media (hover: none)': {
+          backgroundColor: alpha(
+            theme.palette.primary.main,
+            ODD_OPACITY + theme.palette.action.selectedOpacity,
+          ),
+        },
+      },
+    },
+  },
+}));
+
 
 const rows: GridRowsProp = [
-  { id: 1, col1: 'Hello', col2: 'World' },
-  { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-  { id: 3, col1: 'MUI', col2: 'is Amazing' },
+  { id: 1, col1: '1', col2: '장미', col3: '하젤', col4: '특2', col5: '양재', col6: '10', col7: '1000', col8: '픽업대기'},
+  { id: 2, col1: '2', col2: '장미', col3: '스프레이', col4: '특2', col5: '광주', col6: '20', col7: '2500', col8: '픽업완료'},
+  { id: 3, col1: '3', col2: '국화', col3: '다이아몬드', col4: '특2', col5: '양재', col6: '20', col7: '12000', col8: '픽업완료'},
+  { id: 4, col1: '4', col2: '수국', col3: '베레나핑크', col4: '중3', col5: '부산', col6: '30', col7: '3800', col8: '픽업완료'},
+  { id: 5, col1: '5', col2: '수국', col3: '베레나핑크', col4: '상3', col5: '부산', col6: '5', col7: '4950', col8: '픽업완료'},
 ];
 
 const columns: GridColDef[] = [
-  { field: 'col1', headerName: 'Column 1', width: 150 },
-  { field: 'col2', headerName: 'Column 2', width: 150 },
+  { field: 'col1', headerName: 'No', width: 150 },
+  { field: 'col2', headerName: '품목', width: 150 },
+  { field: 'col3', headerName: '품종', width: 150 },
+  { field: 'col4', headerName: '등급', width: 150 },
+  { field: 'col5', headerName: '지역', width: 150 },
+  { field: 'col6', headerName: '단수', width: 150 },
+  { field: 'col7', headerName: '가격', width: 150 },
+  { field: 'col8', headerName: '픽업 현황', width: 150 },
 ];
+
 
 function MyAuctionList() {
 
   return (
     <MDBCard>
       <MDBCardBody>
-        <DataGrid rows={rows} columns={columns} />
+        <MDBCardTitle className="mb-4 font-bold">
+          나의 낙찰 내역
+        </MDBCardTitle>
+
+        <div style={{ height: 400, width: '100%' }}>
+          <StripedDataGrid
+            rows={rows} columns={columns}
+            getRowClassName={(params) =>
+              params.indexRelativeToCurrentPage % 2 === 0 ? 'even' : 'odd'
+            }
+          />
+        </div>
+
       </MDBCardBody>
     </MDBCard>
   )
