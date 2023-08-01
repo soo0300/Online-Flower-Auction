3d97f9d3fc1e4c9ccb7e87c88abfd6fe7f84d918
임우택 <lyt1228@naver.com>
2023-08-01 14:22:00 +0900

    🎉feat: S09P12C204-320 카테고리 조회 서비스 구현

---
+++ admin/src/main/java/com/kkoch/admin/api/service/category/CategoryQueryService.java
@@ -0,0 +1,28 @@
+package com.kkoch.admin.api.service.category;
+
+import com.kkoch.admin.api.controller.category.response.CategoryForMemberResponse;
+import com.kkoch.admin.domain.plant.Category;
+import com.kkoch.admin.domain.plant.repository.CategoryQueryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class CategoryQueryService {
+
+
+    private final CategoryQueryRepository categoryQueryRepository;
+
+    public List<CategoryForMemberResponse> getCategories(String parentName) {
+        List<Category> categories = categoryQueryRepository.getCategories(parentName);
+
+        return categories.stream()
+            .map(CategoryForMemberResponse::of)
+            .collect(Collectors.toList());
+    }
+}
