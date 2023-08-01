c3e2bd5a60f22d0aedbdaad9bee0a581250ee84b
임우택 <lyt1228@naver.com>
2023-08-01 17:18:26 +0900

    ♻️refactor: S09P12C204-356 관리자 컨트롤러 분리

--- admin/src/main/java/com/kkoch/admin/api/controller/admin/AdminController.java
+++ admin/src/main/java/com/kkoch/admin/api/controller/admin/AdminController.java
@@ -1,68 +1,38 @@
 package com.kkoch.admin.api.controller.admin;
 
-import com.kkoch.admin.api.ApiResponse;
-import com.kkoch.admin.api.controller.admin.request.AddAdminRequest;
-import com.kkoch.admin.api.controller.admin.request.EditAdminRequest;
 import com.kkoch.admin.api.controller.admin.request.LoginRequest;
-import com.kkoch.admin.api.controller.admin.response.AdminResponse;
 import com.kkoch.admin.api.service.admin.AdminService;
-import com.kkoch.admin.api.service.admin.dto.AddAdminDto;
-import com.kkoch.admin.api.service.admin.dto.EditAdminDto;
 import com.kkoch.admin.api.service.admin.dto.LoginDto;
+import com.kkoch.admin.login.Login;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpSession;
-import java.util.List;
 
-import static org.springframework.http.HttpStatus.MOVED_PERMANENTLY;
-
-@RequestMapping("/admin-service/admin")
 @RequiredArgsConstructor
-@RestController
+@Controller
+@Slf4j
 public class AdminController {
 
     private final AdminService adminService;
 
-    //관계자 전체 조회
     @GetMapping
-    public ApiResponse<List<AdminResponse>> getAdminList() {
-        List<AdminResponse> adminList = adminService.getAdminList();
-        return ApiResponse.ok(adminList);
+    public String index(@ModelAttribute(name = "form") LoginRequest request, @Login LoginAdmin loginAdmin) {
+        if (loginAdmin == null) {
+            return "index";
+        }
+        return "dashboard";
     }
 
-    //관계자 등록
-    @PostMapping
-    public ApiResponse<Long> addAdmin(@RequestBody AddAdminRequest request) {
-        AddAdminDto dto = request.toAddAdminDto();
-        Long adminId = adminService.addAdmin(dto);
-        return ApiResponse.ok(adminId);
-
-    }
-
-    //관게자 수정
-    @PatchMapping("/{adminId}")
-    public ApiResponse<Long> setAdmin(@PathVariable Long adminId, @RequestBody EditAdminRequest request) {
-        EditAdminDto dto = request.toEditAdminDto();
-        Long setId = adminService.setAdmin(adminId, dto);
-        return ApiResponse.ok(setId);
-
-    }
-
-    //관계자 삭제
-    @DeleteMapping("/{adminId}")
-    public ApiResponse<Long> removeAdmin(@PathVariable Long adminId) {
-        Long deleteId = adminService.removeAdmin(adminId);
-        return ApiResponse.of(MOVED_PERMANENTLY, "관계자 정보가 삭제되었습니다.", deleteId);
-    }
-
-    //관계자 로그인
     @PostMapping("/login")
-    public ApiResponse<String> login(@RequestBody LoginRequest request, HttpSession session) {
+    public String login(@ModelAttribute(name = "form") LoginRequest request, HttpSession session) {
         LoginDto dto = request.toLoginDto();
         LoginAdmin loginAdmin = adminService.loginAdmin(dto);
         session.setAttribute("loginAdmin", loginAdmin);
-        return ApiResponse.ok("Login success!");
+        return "redirect:/";
     }
-
 }
