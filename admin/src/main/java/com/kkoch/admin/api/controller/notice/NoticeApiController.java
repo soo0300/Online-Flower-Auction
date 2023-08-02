146463a24b33b1a167f5d4e6c96db74bec98f2ec
dong82 <lyt1228@naver.com>
2023-08-02 21:41:06 +0900

    ♻️refactor: S09P12C204-357 공지사항 목록 조회 컨트롤러 수정

--- admin/src/main/java/com/kkoch/admin/api/controller/notice/NoticeApiController.java
+++ admin/src/main/java/com/kkoch/admin/api/controller/notice/NoticeApiController.java
@@ -2,17 +2,16 @@ package com.kkoch.admin.api.controller.notice;
 
 import com.kkoch.admin.api.ApiResponse;
 import com.kkoch.admin.api.controller.notice.response.NoticeResponse;
-import com.kkoch.admin.domain.notice.repository.NoticeQueryRepository;
+import com.kkoch.admin.api.service.notice.NoticeQueryService;
 import com.kkoch.admin.domain.notice.repository.dto.NoticeSearchCond;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -20,10 +19,10 @@ import java.util.List;
 @RequestMapping("/admin-service/notices")
 public class NoticeApiController {
 
-    private final NoticeQueryRepository noticeQueryRepository;
+    private final NoticeQueryService noticeQueryService;
 
     @GetMapping
-    public ApiResponse<List<NoticeResponse>> getNotices(
+    public ApiResponse<Page<NoticeResponse>> getNotices(
             @RequestParam Integer type,
             @RequestParam String keyword,
             @RequestParam Integer pageNum
@@ -32,7 +31,7 @@ public class NoticeApiController {
 
         PageRequest request = PageRequest.of(pageNum - 1, 10);
 
-        List<NoticeResponse> content = noticeQueryRepository.getNoticeByCondition(cond, request);
+        Page<NoticeResponse> content = noticeQueryService.getNotices(cond, request);
 
         return ApiResponse.ok(content);
     }
