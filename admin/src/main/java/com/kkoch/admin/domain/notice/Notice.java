0a1dc9e2d2d9dff28046a0314a5c1bc580525544
dong82 <lyt1228@naver.com>
2023-08-02 21:02:47 +0900

    🎉feat: S09P12C204-357 공지사항 엔티티 생성

---
+++ admin/src/main/java/com/kkoch/admin/domain/notice/Notice.java
@@ -0,0 +1,39 @@
+package com.kkoch.admin.domain.notice;
+
+import com.kkoch.admin.domain.TimeBaseEntity;
+import com.kkoch.admin.domain.admin.Admin;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Notice extends TimeBaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "notice_id")
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "admin_id")
+    private Admin admin;
+
+    @Builder
+    private Notice(String title, String content, Admin admin) {
+        this.title = title;
+        this.content = content;
+        this.admin = admin;
+    }
+}
