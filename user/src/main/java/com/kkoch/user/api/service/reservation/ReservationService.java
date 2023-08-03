37bc627d1274f659c0dc41cfce10908c30701ded
김수진 <soo0300@naver.com>
2023-08-03 09:43:48 +0900

    🎉feat: S09P12C204-339-거래 예약 조회 서비스단 구현

--- user/src/main/java/com/kkoch/user/api/service/reservation/ReservationService.java
+++ user/src/main/java/com/kkoch/user/api/service/reservation/ReservationService.java
@@ -24,12 +24,8 @@ public class ReservationService {
     public Long addReservation(String loginID, AddReservationDto dto) {
         Member member = memberRepository.findByEmail(loginID)
                 .orElseThrow(() -> new NoSuchElementException("존재하지 않는 거래 예약입니다."));
-
-        //memberId를 포함하여 엔티티로 만들어 준다.
         Reservation reservation = dto.toEntity(member);
-
         Reservation savedReservation = reservationRepository.save(reservation);
-
         return savedReservation.getId();
     }
 
@@ -37,14 +33,16 @@ public class ReservationService {
     public Long removeReservation(Long reservationId) {
         Reservation reservation = reservationRepository.findById(reservationId)
                 .orElseThrow(() -> new IllegalArgumentException("삭제할 수 없는 거래 예약입니다."));
-
         reservationRepository.delete(reservation);
-
         return reservation.getId();
     }
 
     //거래 예약 조회
     public Reservation getReservation(Long memberId) {
-        return null;
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("조회할 수 없는 회원입니다."));
+        Reservation reservation = reservationRepository.findByMember(member)
+                .orElseThrow(() -> new IllegalArgumentException("조회할 수 없는  거래 예약입니다."));
+        return reservation;
     }
 }
