246ae5e074dd05bba3fa3f1495a4a62c0b161d1b
dong82 <lyt1228@naver.com>
2023-08-03 20:36:08 +0900

    🎉feat: S09P12C204-359 loadUserByUsername() 구현

--- user/src/main/java/com/kkoch/user/api/service/member/MemberService.java
+++ user/src/main/java/com/kkoch/user/api/service/member/MemberService.java
@@ -4,14 +4,20 @@ import com.kkoch.user.api.service.member.dto.JoinMemberDto;
 import com.kkoch.user.domain.member.Member;
 import com.kkoch.user.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder passwordEncoder;
@@ -25,17 +31,17 @@ public class MemberService {
         return savedMember.getId();
     }
 
-//    public TokenResponse login(LoginMemberDto dto) {
-//        Member member = memberRepository.findByEmail(dto.getEmail()).orElseThrow(() ->
-//                new BadCredentialsException("잘못된 계정정보입니다."));
-//
-//        if (!isSamePw(dto.getLoginPw(),member.getLoginPw())) {
-//            throw new BadCredentialsException("잘못된 계정정보입니다.");
-//        }
-//        return new TokenResponse(jwtProvider.createToken(member.getEmail(), member.getRoles()));
-//    }
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<Member> findMember = memberRepository.findByEmail(email);
 
-    public boolean isSamePw(String dtoPW, String domainPw){
-        return dtoPW.equals(domainPw);
+        if (findMember.isEmpty()) {
+            throw new UsernameNotFoundException("등록되지 않는 사용자입니다.");
+        }
+
+        Member member = findMember.get();
+        return new User(member.getEmail(), member.getEncryptedPwd(),
+            true, true, true, true,
+            new ArrayList<>()); //권한
     }
 }
--- user/src/main/java/com/kkoch/user/security/WebSecurity.java
+++ user/src/main/java/com/kkoch/user/security/WebSecurity.java
@@ -1,20 +1,47 @@
 package com.kkoch.user.security;
 
+import com.kkoch.user.api.service.member.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configurable
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
+    private final MemberService memberService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private Environment env;
+
+    //권한 관련
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/users/**").permitAll();
+        http.authorizeRequests().antMatchers("/**")
+            .hasIpAddress("192.168.0.16") //ip 변경
+            .and()
+            .addFilter(getAuthenticationFilter());
 
         //h2 db frame
         http.headers().frameOptions().disable();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+
+        return authenticationFilter;
+    }
+
+    //인증
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(memberService).passwordEncoder(bCryptPasswordEncoder);
     }
 }
