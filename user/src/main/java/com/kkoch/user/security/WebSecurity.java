8e91056dcbbd9a34a3e0f7ff6aadcd36b6df07e5
dong82 <lyt1228@naver.com>
2023-08-04 08:47:25 +0900

    ♻️refactor: S09P12C204-359 시큐리티 버전 업에 따른 코드 수정

--- user/src/main/java/com/kkoch/user/security/WebSecurity.java
+++ user/src/main/java/com/kkoch/user/security/WebSecurity.java
@@ -3,46 +3,46 @@ package com.kkoch.user.security;
 import com.kkoch.user.api.service.member.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configurable
 @EnableWebSecurity
 @RequiredArgsConstructor
-public class WebSecurity extends WebSecurityConfigurerAdapter {
+public class WebSecurity {
 
     private final MemberService memberService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final Environment env;
 
-    //권한 관련
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    AuthenticationManager authenticationManager;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(memberService).passwordEncoder(bCryptPasswordEncoder);
+        authenticationManager = authenticationManagerBuilder.build();
+
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter(authenticationManager, memberService, env);
+
         http.csrf().disable();
-        http.authorizeRequests().antMatchers("/**")
-            .hasIpAddress("192.168.0.16") //ip 변경
+
+        http.authorizeRequests()
+            .antMatchers("/error/**", "/join").permitAll()
+            .antMatchers("/**")
+            .hasIpAddress("172.0.0.1")
             .and()
-            .addFilter(getAuthenticationFilter());
+            .authenticationManager(authenticationManager)
+            .addFilter(authenticationFilter);
 
-        //h2 db frame
         http.headers().frameOptions().disable();
-    }
 
-    private AuthenticationFilter getAuthenticationFilter() throws Exception {
-        AuthenticationFilter authenticationFilter =
-            new AuthenticationFilter(authenticationManager(), memberService, env);
-//        authenticationFilter.setAuthenticationManager(authenticationManager());
-
-        return authenticationFilter;
-    }
-
-    //인증
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(memberService).passwordEncoder(bCryptPasswordEncoder);
+        return http.build();
     }
 }
