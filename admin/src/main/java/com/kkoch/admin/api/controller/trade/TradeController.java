e483b378f41dde4c2d0ee4063c340b7848e9ede5
dong82 <lyt1228@naver.com>
2023-08-05 18:21:13 +0900

    🎉feat: S09P12C204-362 msa

--- admin/src/main/java/com/kkoch/admin/api/controller/trade/TradeController.java
+++ admin/src/main/java/com/kkoch/admin/api/controller/trade/TradeController.java
@@ -9,8 +9,6 @@ import com.kkoch.admin.api.service.trade.TradeQueryService;
 import com.kkoch.admin.api.service.trade.TradeService;
 import com.kkoch.admin.api.service.trade.dto.AddTradeDto;
 import com.kkoch.admin.domain.trade.repository.dto.TradeSearchCond;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -28,13 +26,11 @@ import static org.springframework.http.HttpStatus.MOVED_PERMANENTLY;
 @RestController
 @RequestMapping("/admin-service/trades")
 @Slf4j
-@Api(tags = {"낙찰 내역 기능"})
 public class TradeController {
 
     private final TradeService tradeService;
     private final TradeQueryService tradeQueryService;
 
-    @ApiOperation(value = "낙찰 내역 등록")
     @PostMapping
     public ApiResponse<Long> addTrade(@Valid @RequestBody AddTradeRequest request) {
         List<AddTradeDto> dto = toAddTradeDto(request);
@@ -44,7 +40,6 @@ public class TradeController {
         return ApiResponse.ok(tradeId);
     }
 
-    @ApiOperation(value = "낙찰 내역 조회")
     @GetMapping("/{memberId}")
     public ApiResponse<Page<TradeResponse>> getMyTrades(
             @PathVariable Long memberId,
@@ -57,21 +52,18 @@ public class TradeController {
         return ApiResponse.ok(data);
     }
 
-    @ApiOperation(value = "낙찰 내역 상세 조회")
     @GetMapping("/detail/{tradeId}")
     public ApiResponse<TradeDetailResponse> getTrade(@PathVariable Long tradeId) {
         TradeDetailResponse response = tradeQueryService.getTrade(tradeId);
         return ApiResponse.ok(response);
     }
 
-    @ApiOperation(value = "낙찰 픽업여부 변경")
     @PatchMapping("/{tradeId}")
     public ApiResponse<Long> pickup(@PathVariable Long tradeId) {
         Long pickupTradeId = tradeService.pickup(tradeId);
         return ApiResponse.ok(pickupTradeId);
     }
 
-    @ApiOperation(value = "낙찰 내역 삭제")
     @DeleteMapping("/{tradeId}")
     public ApiResponse<Long> removeTrade(@PathVariable Long tradeId) {
         Long removedId = tradeService.remove(tradeId);
