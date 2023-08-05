8d9a0ed2ae7c513eff73a013bd862e712375aad3
dong82 <lyt1228@naver.com>
2023-08-05 14:16:54 +0900

    🎉feat: S09P12C204-362 FeignErrorDecoder 구현

--- user/src/main/java/com/kkoch/user/UserApplication.java
+++ user/src/main/java/com/kkoch/user/UserApplication.java
@@ -1,5 +1,6 @@
 package com.kkoch.user;
 
+import feign.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -19,5 +20,10 @@ public class UserApplication {
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public Logger.Level feignLoggerLevel() {
+		return Logger.Level.FULL;
 	}
 }
