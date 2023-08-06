e8bf988a009dfc726bcef9e0c4c886da3d25bbe6
dong82 <lyt1228@naver.com>
2023-08-06 21:07:35 +0900

    ♻️refactor: 관리자 인트라넷 경로 수정

--- admin/src/main/java/com/kkoch/admin/api/controller/admin/AdminController.java
+++ admin/src/main/java/com/kkoch/admin/api/controller/admin/AdminController.java
@@ -15,6 +15,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -22,6 +23,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @Controller
 @Slf4j
+@RequestMapping("/admin-service/intranet")
 public class AdminController {
 
     private final AdminService adminService;
