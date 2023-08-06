b9fc40da76465a2d5c923fd91c5b8e3de667a2ba
sungjoo <hans52410537@gmail.com>
2023-08-07 00:41:10 +0900

    🎉 feat: S09P12C204-286 낙찰현황조회/삭제 기능

--- kkoch/vite.config.ts
+++ kkoch/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://i9c204.p.ssafy.io',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       },
