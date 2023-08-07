d9721c9aac1255f3975dd23f9cb219c711dce48b
dong82 <lyt1228@naver.com>
2023-08-08 01:05:32 +0900

    🎉feat: S09P12C204-468 거래 예약 조회 컨트롤러 구현

--- user/src/main/java/com/kkoch/user/api/controller/reservation/ReservationController.java
+++ user/src/main/java/com/kkoch/user/api/controller/reservation/ReservationController.java
@@ -2,11 +2,15 @@ package com.kkoch.user.api.controller.reservation;
 
 import com.kkoch.user.api.controller.ApiResponse;
 import com.kkoch.user.api.controller.reservation.request.AddReservationRequest;
+import com.kkoch.user.api.controller.reservation.response.AddReservationResponse;
 import com.kkoch.user.api.controller.reservation.response.ReservationResponse;
+import com.kkoch.user.api.service.reservation.ReservationQueryService;
 import com.kkoch.user.api.service.reservation.ReservationService;
 import com.kkoch.user.api.service.reservation.dto.AddReservationDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -18,15 +22,26 @@ import javax.validation.Valid;
 public class ReservationController {
 
     private final ReservationService reservationService;
+    private final ReservationQueryService reservationQueryService;
 
     @PostMapping
-    public ApiResponse<ReservationResponse> addReservation(
+    public ApiResponse<AddReservationResponse> addReservation(
         @Valid @RequestBody AddReservationRequest request,
         @PathVariable String memberKey
     ) {
         AddReservationDto dto = request.toAddReservationDto();
-        ReservationResponse response = reservationService.addReservation(memberKey, dto);
+        AddReservationResponse response = reservationService.addReservation(memberKey, dto);
 
+        return ApiResponse.ok(response);
+    }
+
+    @GetMapping
+    public ApiResponse<Page<ReservationResponse>> reservations(
+        @PathVariable String memberKey,
+        @RequestParam(defaultValue = "0") Integer pageNum
+    ) {
+        PageRequest pageRequest = PageRequest.of(pageNum, 10);
+        Page<ReservationResponse> response = reservationQueryService.getMyReservations(memberKey, pageRequest);
         return ApiResponse.ok(response);
     }
 }
