a5f5de8429309d8dac8a314b7f453ae0ae7bf9f3
dong82 <lyt1228@naver.com>
2023-08-07 21:11:27 +0900

    🎉feat: S09P12C204-468 회원정보조회 쿼리 구현

---
+++ user/src/main/java/com/kkoch/user/domain/member/repository/MemberQueryRepository.java
@@ -0,0 +1,34 @@
+package com.kkoch.user.domain.member.repository;
+
+import com.kkoch.user.api.controller.member.response.MemberInfoResponse;
+import com.kkoch.user.domain.member.QMember;
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+import static com.kkoch.user.domain.member.QMember.*;
+
+@Repository
+public class MemberQueryRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    public MemberQueryRepository(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    public MemberInfoResponse findMyInfoByMemberKey(String memberKey) {
+        return queryFactory
+            .select(Projections.constructor(MemberInfoResponse.class,
+                member.email,
+                member.name,
+                member.tel,
+                member.businessNumber
+            ))
+            .from(member)
+            .where(member.memberKey.eq(memberKey))
+            .fetchFirst();
+    }
+}
