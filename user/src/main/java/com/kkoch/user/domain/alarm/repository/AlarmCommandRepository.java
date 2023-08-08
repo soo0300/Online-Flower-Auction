5a511e228f493cfc673ba07c7f400d5380416da6
dong82 <lyt1228@naver.com>
2023-08-08 20:21:21 +0900

    ♻️refactor: S09P12C204-473 알림 열람 업데이트 쿼리 수정

--- user/src/main/java/com/kkoch/user/domain/alarm/repository/AlarmCommandRepository.java
+++ user/src/main/java/com/kkoch/user/domain/alarm/repository/AlarmCommandRepository.java
@@ -1,12 +1,16 @@
 package com.kkoch.user.domain.alarm.repository;
 
 import com.kkoch.user.domain.alarm.QAlarm;
+import com.kkoch.user.domain.member.QMember;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 
+import java.util.List;
+
 import static com.kkoch.user.domain.alarm.QAlarm.alarm;
+import static com.kkoch.user.domain.member.QMember.*;
 
 @Repository
 public class AlarmCommandRepository {
@@ -17,14 +21,22 @@ public class AlarmCommandRepository {
         this.queryFactory = new JPAQueryFactory(em);
     }
 
-    public int updateOpen(Long memberId) {
+    public int updateOpen(String memberKey) {
+        List<Long> alarmIds = queryFactory
+            .select(alarm.id)
+            .from(alarm)
+            .join(alarm.member, member)
+            .where(
+                alarm.open.isFalse(),
+                member.memberKey.eq(memberKey)
+            )
+            .fetch();
+
         return (int) queryFactory
             .update(alarm)
-            .where(
-                alarm.member.id.eq(memberId),
-                alarm.open.isFalse()
-            )
+            .where(alarm.id.in(alarmIds))
             .set(alarm.open, true)
             .execute();
     }
+
 }
