82cfe811102b0b7311188990de0e583cb2059f8c
dong82 <lyt1228@naver.com>
2023-08-08 20:21:53 +0900

    ♻️refactor: S09P12C204-473 알림 열람 업데이트 서비스 수정

--- user/src/main/java/com/kkoch/user/api/service/alarm/AlamService.java
+++ user/src/main/java/com/kkoch/user/api/service/alarm/AlamService.java
@@ -14,10 +14,7 @@ public class AlamService {
     private final AlarmCommandRepository alarmCommandRepository;
     private final MemberRepository memberRepository;
 
-    public int open(String email) {
-        Long memberId = memberRepository.findIdByEmail(email)
-            .orElseThrow();
-
-        return alarmCommandRepository.updateOpen(memberId);
+    public int open(String memberKey) {
+        return alarmCommandRepository.updateOpen(memberKey);
     }
 }
