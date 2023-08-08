58066e9ffa0a7190e80c3b1d6130b08a18efaa9d
이예리 <leeyr0412@naver.com>
2023-08-08 16:58:18 +0900

    ♻️refactor: S09P12C204-437 QueryRepositoryTest 주석 추가

--- admin/src/test/java/com/kkoch/admin/domain/auction/repository/AuctionArticleRepositoryTest.java
+++ admin/src/test/java/com/kkoch/admin/domain/auction/repository/AuctionArticleRepositoryTest.java
@@ -6,12 +6,14 @@ import com.kkoch.admin.domain.auction.AuctionArticle;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
+@Transactional
 class AuctionArticleRepositoryTest extends IntegrationTestSupport {
 
     @Autowired
