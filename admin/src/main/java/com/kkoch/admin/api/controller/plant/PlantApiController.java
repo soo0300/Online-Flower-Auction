978240e5abbcec033fd466601eeea59f93f916ad
홍승준 <ghdgn22@gmail.com>
2023-08-08 15:04:16 +0900

    ♻️refactor: S09P12C204-375-관리자 페이지를 위한 식물 컨트롤러 분리

---
+++ admin/src/main/java/com/kkoch/admin/api/controller/plant/PlantApiController.java
@@ -0,0 +1,36 @@
+package com.kkoch.admin.api.controller.plant;
+
+import com.kkoch.admin.api.ApiResponse;
+import com.kkoch.admin.api.controller.plant.request.GetPlantRequest;
+import com.kkoch.admin.api.controller.plant.response.PlantResponse;
+import com.kkoch.admin.api.service.plant.PlantQueryService;
+import com.kkoch.admin.api.service.plant.PlantService;
+import com.kkoch.admin.api.service.plant.dto.PlantSearchCond;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/admin-service/plants")
+@RestController
+public class PlantApiController {
+
+    private final PlantQueryService plantQueryService;
+
+    private final PlantService plantService;
+
+    @GetMapping("/{plantId}")
+    public ApiResponse<List<PlantResponse>> getPlant(@RequestBody GetPlantRequest request) {
+        PlantSearchCond cond = request.toSearchCond();
+        return ApiResponse.ok(plantQueryService.getPlant(cond));
+    }
+
+    @GetMapping("/reservation")
+    public Long getPlantId(
+            @RequestParam String type,
+            @RequestParam String name
+    ) {
+        return plantQueryService.getPlantId(type, name);
+    }
+}
--- admin/src/main/java/com/kkoch/admin/api/controller/plant/PlantController.java
+++ admin/src/main/java/com/kkoch/admin/api/controller/plant/PlantController.java
@@ -1,33 +1,28 @@
 package com.kkoch.admin.api.controller.plant;
 
-import com.kkoch.admin.api.ApiResponse;
-import com.kkoch.admin.api.controller.plant.request.GetPlantRequest;
 import com.kkoch.admin.api.controller.plant.response.PlantResponse;
 import com.kkoch.admin.api.service.plant.PlantQueryService;
-import com.kkoch.admin.api.service.plant.dto.PlantSearchCond;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@RequestMapping("/admin-service/plants")
-@RestController
+@Controller
+@Slf4j
+@RequestMapping("/admin-service/intranet")
 public class PlantController {
 
     private final PlantQueryService plantQueryService;
 
-    @GetMapping
-    public ApiResponse<List<PlantResponse>> getPlants(@RequestBody GetPlantRequest request) {
-        PlantSearchCond cond = request.toSearchCond();
-        return ApiResponse.ok(plantQueryService.getPlants(cond));
-    }
-
-    @GetMapping("/reservation")
-    public Long getPlantId(
-        @RequestParam String type,
-        @RequestParam String name
-    ) {
-        return plantQueryService.getPlantId(type, name);
+    @GetMapping("/plants")
+    public String plantPage(Model model) {
+        List<PlantResponse> responses = plantQueryService.getPlants();
+        model.addAttribute("plants", responses);
+        return "category";
     }
 }
