41c46d786c0db7295c18b8c25c01856a1a3f5d9b
dong82 <lyt1228@naver.com>
2023-08-08 19:03:10 +0900

    🎉feat: S09P12C204-473 쿼리 오류 수정

--- user/src/main/java/com/kkoch/user/domain/member/repository/MemberRepository.java
+++ user/src/main/java/com/kkoch/user/domain/member/repository/MemberRepository.java
@@ -16,6 +16,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     @Query("select m.id from Member m where m.email =:email")
     Optional<Long> findIdByEmail(@Param("email") String email);
 
-    @Query("select m from Member m where m.memberKey =: memberKey")
+    @Query("select m from Member m where m.memberKey =:memberKey")
     Optional<Member> findByMemberKey(@Param("memberKey") String memberKey);
 }
