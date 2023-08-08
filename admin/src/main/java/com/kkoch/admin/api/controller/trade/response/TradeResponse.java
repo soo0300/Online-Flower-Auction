6134418681ec47644df59b38604436e9f2bf348c
임우택 <lyt1228@naver.com>
2023-08-08 15:35:30 +0900

    🎉feat: S09P12C204-473 낙찰 내역 조회 서비스 구현

--- admin/src/main/java/com/kkoch/admin/api/controller/trade/response/TradeResponse.java
+++ admin/src/main/java/com/kkoch/admin/api/controller/trade/response/TradeResponse.java
@@ -1,5 +1,6 @@
 package com.kkoch.admin.api.controller.trade.response;
 
+import com.kkoch.admin.domain.Grade;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,18 +10,23 @@ import java.time.format.DateTimeFormatter;
 @Data
 public class TradeResponse {
 
-    private Long tradeId;
-    private int totalPrice;
-    private String tradeDate;
-    private boolean pickupStatus;
+    private String type;
+    private String name;
+
+    private String grade;
     private int count;
+    private int bidPrice;
+    private String bidTime;
+    private String region;
 
     @Builder
-    public TradeResponse(Long tradeId, int totalPrice, LocalDateTime tradeDate, boolean pickupStatus, int count) {
-        this.tradeId = tradeId;
-        this.totalPrice = totalPrice;
-        this.tradeDate = tradeDate.format(DateTimeFormatter.ofPattern("yyyy.MM.dd"));
-        this.pickupStatus = pickupStatus;
+    public TradeResponse(String type, String name, Grade grade, int count, int bidPrice, LocalDateTime bidTime, String region) {
+        this.type = type;
+        this.name = name;
+        this.grade = grade.getText();
         this.count = count;
+        this.bidPrice = bidPrice;
+        this.bidTime = bidTime.format(DateTimeFormatter.ofPattern("yyyy.MM.dd hh:mm"));
+        this.region = region;
     }
 }
