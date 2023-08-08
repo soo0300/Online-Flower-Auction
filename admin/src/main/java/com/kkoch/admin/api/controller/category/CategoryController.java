0ba14e1e8b0da47a23522046cf9659388e8342a6
이예리 <leeyr0412@naver.com>
2023-08-08 23:28:43 +0900

    🎉feat: S09P12C204-440 CategoryController 로그 추가

--- admin/src/main/java/com/kkoch/admin/api/controller/category/CategoryController.java
+++ admin/src/main/java/com/kkoch/admin/api/controller/category/CategoryController.java
@@ -28,7 +28,7 @@ public class CategoryController {
 
     @PostMapping
     public ApiResponse<Long> addCategory(@Valid @RequestBody AddCategoryRequest request) {
-
+        log.info("<카테고리 등록> Controller");
         AddCategoryDto dto = request.toAddCategoryDto();
         Long result = categoryService.addCategory(dto);
 
@@ -39,6 +39,7 @@ public class CategoryController {
     public ApiResponse<List<String>> getTypes(
             @RequestParam(defaultValue = "절화") String code
     ) {
+        log.info("<카테고리 품목 조회> Controller");
         List<String> types = categoryQueryService.getTypesForMember(code);
         return ApiResponse.ok(types);
     }
@@ -49,12 +50,14 @@ public class CategoryController {
             @RequestParam(defaultValue = "") String type
             //날짜
     ) {
+        log.info("<카테고리 품종 조회> Controller");
         List<String> names = categoryQueryService.getNamesForMember(code, type);
         return ApiResponse.ok(names);
     }
 
     @GetMapping("/{parentId}")
     public ApiResponse<List<CategoryResponse>> getCategoriesParentId(@PathVariable Long parentId) {
+        log.info("<카테고리 하위 카테고리 조회> Controller");
         List<CategoryResponse> subCategories = categoryQueryService.getCategoriesByParentId(parentId);
         return ApiResponse.ok(subCategories);
     }
@@ -62,7 +65,7 @@ public class CategoryController {
     @PatchMapping("/{categoryId}")
     public ApiResponse<CategoryResponse> setCategory(@PathVariable Long categoryId
             , @Valid @RequestBody SetCategoryRequest request) {
-
+        log.info("<카테고리 수정> Controller");
         SetCategoryDto dto = request.toSetCategoryDto();
         CategoryResponse result = categoryService.setCategory(categoryId, dto);
         return ApiResponse.of(HttpStatus.MOVED_PERMANENTLY, "카테고리가 수정되었습니다.", result);
@@ -70,6 +73,7 @@ public class CategoryController {
 
     @DeleteMapping("/{categoryId}")
     public ApiResponse<Long> removeCategory(@PathVariable Long categoryId) {
+        log.info("<카테고리 삭제> Controller");
         Long removeId = categoryService.removeCategory(categoryId);
         return ApiResponse.of(HttpStatus.MOVED_PERMANENTLY, "카테고리가 삭제되었습니다.", removeId);
 
