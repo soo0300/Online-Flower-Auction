5c3e70e8dec09015be3a3f10485fcfe57924f4f6
이예리 <leeyr0412@naver.com>
2023-08-08 22:59:11 +0900

    🎉feat: S09P12C204-440 Admin api 로그 추가

--- admin/src/main/java/com/kkoch/admin/api/controller/admin/AdminApiController.java
+++ admin/src/main/java/com/kkoch/admin/api/controller/admin/AdminApiController.java
@@ -6,6 +6,7 @@ import com.kkoch.admin.api.service.admin.AdminQueryService;
 import com.kkoch.admin.api.service.admin.AdminService;
 import com.kkoch.admin.api.service.admin.dto.EditAdminDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpStatus.MOVED_PERMANENTLY;
@@ -13,6 +14,7 @@ import static org.springframework.http.HttpStatus.MOVED_PERMANENTLY;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/admin-service/admins")
+@Slf4j
 public class AdminApiController {
 
     private final AdminService adminService;
@@ -21,6 +23,7 @@ public class AdminApiController {
     //관게자 수정
     @PatchMapping("/{adminId}")
     public ApiResponse<Long> setAdmin(@PathVariable Long adminId, @RequestBody EditAdminRequest request) {
+        log.info("<관계자 정보 수정> Controller");
         EditAdminDto dto = request.toEditAdminDto();
         Long setId = adminService.setAdmin(adminId, dto);
         return ApiResponse.ok(setId);
@@ -30,6 +33,7 @@ public class AdminApiController {
     //관계자 삭제
     @DeleteMapping("/{adminId}")
     public ApiResponse<Long> removeAdmin(@PathVariable Long adminId) {
+        log.info("<관계자 삭제> Controller");
         Long deleteId = adminService.removeAdmin(adminId);
         return ApiResponse.of(MOVED_PERMANENTLY, "관계자 정보가 삭제되었습니다.", deleteId);
     }
