4d5dc222d431722628aef1a4ea76dbdfab105c72
이예리 <leeyr0412@naver.com>
2023-08-09 11:38:53 +0900

    ♻️refactor: S09P12C204-440 AuctionArticleQuery 리팩토링

--- admin/src/main/java/com/kkoch/admin/domain/auction/repository/AuctionArticleQueryRepository.java
+++ admin/src/main/java/com/kkoch/admin/domain/auction/repository/AuctionArticleQueryRepository.java
@@ -1,13 +1,11 @@
 package com.kkoch.admin.domain.auction.repository;
 
-import com.kkoch.admin.api.controller.auction.response.AuctionArticleForMemberResponse;
 import com.kkoch.admin.api.controller.auction.response.AuctionArticlePeriodSearchResponse;
 import com.kkoch.admin.api.controller.auction.response.AuctionArticlesForAdminResponse;
 import com.kkoch.admin.api.controller.auction.response.AuctionArticlesResponse;
 import com.kkoch.admin.api.controller.trade.response.SuccessfulBid;
 import com.kkoch.admin.api.controller.trade.response.TradeResponse;
 import com.kkoch.admin.domain.auction.repository.dto.AuctionArticlePeriodSearchCond;
-import com.kkoch.admin.domain.auction.repository.dto.AuctionArticleSearchCond;
 import com.kkoch.admin.domain.auction.repository.dto.AuctionArticleSearchForAdminCond;
 import com.kkoch.admin.domain.plant.QCategory;
 import com.querydsl.core.types.dsl.BooleanExpression;
@@ -35,6 +33,79 @@ public class AuctionArticleQueryRepository {
 
     public AuctionArticleQueryRepository(EntityManager em) {
         this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    /* 경매품 목록조회(기간별 조회) */
+    public List<AuctionArticlePeriodSearchResponse> getAuctionArticleListForPeriodSearch(AuctionArticlePeriodSearchCond cond, Pageable pageable) {
+        QCategory code = new QCategory("code");
+        QCategory type = new QCategory("type");
+        QCategory name = new QCategory("name");
+
+        return getAuctionArticleByPeriodSearchCond(cond, code, name, type)
+                .limit(pageable.getPageSize())
+                .offset(pageable.getOffset())
+                .fetch();
+    }
+
+
+    /* 경매품 목록조회(경매시 목록 출력용) */
+    public List<AuctionArticlesResponse> getAuctionArticleList(Long auctionId) {
+        QCategory code = new QCategory("code");
+        QCategory type = new QCategory("type");
+        QCategory name = new QCategory("name");
+
+        return queryFactory.select(constructor(AuctionArticlesResponse.class,
+                        auctionArticle.id,
+                        auctionArticle.auctionNumber,
+                        auctionArticle.plant.code.name,
+                        auctionArticle.plant.type.name,
+                        auctionArticle.plant.name.name,
+                        auctionArticle.count,
+                        auctionArticle.startPrice,
+                        auctionArticle.grade,
+                        auctionArticle.region,
+                        auctionArticle.shipper))
+                .from(auctionArticle)
+                .join(auctionArticle.plant, plant)
+                .join(plant.code, code)
+                .join(plant.type, type)
+                .join(plant.name, name)
+                .join(auctionArticle.auction, auction)
+                .on(auctionArticle.auction.id.eq(auctionId))
+                .orderBy(auctionArticle.auctionNumber.asc())
+                .fetch();
+    }
+
+    /* 경매품 목록조회(관계자용 전체 조회) */
+    public List<AuctionArticlesForAdminResponse> getAuctionArticleListForAdmin(AuctionArticleSearchForAdminCond cond) {
+        QCategory code = new QCategory("code");
+        QCategory type = new QCategory("type");
+        QCategory name = new QCategory("name");
+
+        return queryFactory.select(constructor(AuctionArticlesForAdminResponse.class,
+                        auctionArticle.plant.code.name,
+                        auctionArticle.plant.type.name,
+                        auctionArticle.plant.name.name,
+                        auctionArticle.grade,
+                        auctionArticle.count,
+                        auctionArticle.bidPrice,
+                        auctionArticle.bidTime,
+                        auctionArticle.region,
+                        auctionArticle.shipper))
+                .from(auctionArticle)
+                .join(auctionArticle.plant, plant)
+                .join(plant.code, code)
+                .join(plant.type, type)
+                .join(plant.name, name)
+                .where(
+                        auctionArticle.plant.code.name.eq(cond.getCode()),
+                        auctionArticle.bidTime.between(cond.getStartDateTime(), cond.getEndDateTime()),
+                        eqPlantName(cond.getName()),
+                        eqPlantType(cond.getType()),
+                        eqAuctionRegion(cond.getRegion()),
+                        eqShipper(cond.getShipper())
+                )
+                .fetch();
     }
 
     public List<TradeResponse> findByTradeIds(String memberKey, Pageable pageable) {
@@ -116,98 +187,6 @@ public class AuctionArticleQueryRepository {
                 .size();
     }
 
-    public List<AuctionArticlesForAdminResponse> getAuctionArticleListForAdmin(AuctionArticleSearchForAdminCond cond) {
-        QCategory code = new QCategory("code");
-        QCategory type = new QCategory("type");
-        QCategory name = new QCategory("name");
-
-        return queryFactory.select(constructor(AuctionArticlesForAdminResponse.class,
-                        auctionArticle.plant.code.name,
-                        auctionArticle.plant.type.name,
-                        auctionArticle.plant.name.name,
-                        auctionArticle.grade,
-                        auctionArticle.count,
-                        auctionArticle.bidPrice,
-                        auctionArticle.bidTime,
-                        auctionArticle.region,
-                        auctionArticle.shipper))
-                .from(auctionArticle)
-                .join(auctionArticle.plant, plant)
-                .join(plant.code, code)
-                .join(plant.type, type)
-                .join(plant.name, name)
-                .where(
-                        auctionArticle.plant.code.name.eq(cond.getCode()),
-                        auctionArticle.bidTime.between(cond.getStartDateTime(), cond.getEndDateTime()),
-                        eqPlantName(cond.getName()),
-                        eqPlantType(cond.getType()),
-                        eqAuctionRegion(cond.getRegion()),
-                        eqShipper(cond.getShipper())
-                )
-                .fetch();
-    }
-
-    // 경매 시작 시 목록
-    public List<AuctionArticlesResponse> getAuctionArticleList(Long auctionId) {
-        QCategory code = new QCategory("code");
-        QCategory type = new QCategory("type");
-        QCategory name = new QCategory("name");
-
-        return queryFactory.select(constructor(AuctionArticlesResponse.class,
-                        auctionArticle.id,
-                        auctionArticle.auctionNumber,
-                        auctionArticle.plant.code.name,
-                        auctionArticle.plant.type.name,
-                        auctionArticle.plant.name.name,
-                        auctionArticle.count,
-                        auctionArticle.startPrice,
-                        auctionArticle.grade,
-                        auctionArticle.region,
-                        auctionArticle.shipper))
-                .from(auctionArticle)
-                .join(auctionArticle.plant, plant)
-                .join(plant.code, code)
-                .join(plant.type, type)
-                .join(plant.name, name)
-                .join(auctionArticle.auction, auction)
-                .on(auctionArticle.auction.id.eq(auctionId))
-                .orderBy(auctionArticle.auctionNumber.asc())
-                .fetch();
-    }
-
-    public List<AuctionArticleForMemberResponse> getAuctionArticleListForMember(AuctionArticleSearchCond cond, Pageable pageable) {
-        QCategory code = new QCategory("code");
-        QCategory type = new QCategory("type");
-        QCategory name = new QCategory("name");
-
-        return getAuctionArticleByCond(cond, code, name, type)
-                .limit(pageable.getPageSize())
-                .offset(pageable.getOffset())
-                .fetch();
-    }
-
-    // TODO: 2023-08-04 리팩토링 해보기
-
-    public List<AuctionArticlePeriodSearchResponse> getAuctionArticleListForPeriodSearch(AuctionArticlePeriodSearchCond cond, Pageable pageable) {
-        QCategory code = new QCategory("code");
-        QCategory type = new QCategory("type");
-        QCategory name = new QCategory("name");
-
-        return getAuctionArticleByPeriodSearchCond(cond, code, name, type)
-                .limit(pageable.getPageSize())
-                .offset(pageable.getOffset())
-                .fetch();
-    }
-
-    public int getTotalCount(AuctionArticleSearchCond cond) {
-        QCategory code = new QCategory("code");
-        QCategory type = new QCategory("type");
-        QCategory name = new QCategory("name");
-        return getAuctionArticleByCond(cond, code, type, name)
-                .fetch()
-                .size();
-    }
-
     public int getTotalCountForPeriod(AuctionArticlePeriodSearchCond cond) {
         QCategory code = new QCategory("code");
         QCategory type = new QCategory("type");
@@ -219,30 +198,6 @@ public class AuctionArticleQueryRepository {
 
     private JPAQuery<AuctionArticlePeriodSearchResponse> getAuctionArticleByPeriodSearchCond(AuctionArticlePeriodSearchCond cond, QCategory code, QCategory type, QCategory name) {
         return queryFactory.select(constructor(AuctionArticlePeriodSearchResponse.class,
-                        auctionArticle.plant.code.name,
-                        auctionArticle.plant.type.name,
-                        auctionArticle.plant.name.name,
-                        auctionArticle.grade,
-                        auctionArticle.count,
-                        auctionArticle.bidPrice,
-                        auctionArticle.bidTime,
-                        auctionArticle.region))
-                .from(auctionArticle)
-                .join(auctionArticle.plant, plant)
-                .join(plant.code, code)
-                .join(plant.type, type)
-                .join(plant.name, name)
-                .where(auctionArticle.bidPrice.gt(0),
-                        auctionArticle.plant.code.name.eq(cond.getCode()),
-                        auctionArticle.bidTime.between(cond.getStartDateTime(), cond.getEndDateTime()),
-                        eqPlantName(cond.getName()),
-                        eqPlantType(cond.getType()),
-                        eqAuctionRegion(cond.getRegion())
-                );
-    }
-
-    private JPAQuery<AuctionArticleForMemberResponse> getAuctionArticleByCond(AuctionArticleSearchCond cond, QCategory code, QCategory type, QCategory name) {
-        return queryFactory.select(constructor(AuctionArticleForMemberResponse.class,
                         auctionArticle.plant.code.name,
                         auctionArticle.plant.type.name,
                         auctionArticle.plant.name.name,
