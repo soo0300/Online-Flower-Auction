7e2e65fd693f0ee7ce00d619dd93aa61d3f69ef1
신성주 <hans52410537@gmail.com>
2023-08-11 00:45:15 +0900

    🎉 feat: S09P12C204-424 관리자 경매방 websocket

--- kkoch/src/components/buttons/LocationButton.tsx
+++ kkoch/src/components/buttons/LocationButton.tsx
@@ -1,18 +1,14 @@
 import { useNavigate } from 'react-router-dom';
 import './LocationButton.css';
-import { useSelector } from 'react-redux';
-import { RootState } from '@/reducer/store';
 
 type Props = {
 	location: React.ReactNode;
 	type: string;
 	auctionArticles: object;
 }
-  
 
 const LocationButton = ({location, type, auctionArticles } : Props) => {
 	const navigate = useNavigate();
-	const auctionSession = useSelector((state:RootState) => state.videoAdmin.auctionSession);
 
 	// 양재 지역만 활성화 하기 위한 handler
   const handleCheck = () => {
@@ -20,16 +16,27 @@ const LocationButton = ({location, type, auctionArticles } : Props) => {
 			if(type==="admin"){
 				navigate("/admin/openSession");
 			}else if(type==="user"){
-				console.log(auctionSession)
-				if (auctionSession) {
-					// 세션에 참여
-					console.log(`세션 ${auctionSession}에 참여합니다.`);
-					// 세션 참여 로직 작성
-					navigate("/auction/waitingroom", { state: auctionArticles })
-				} else {
-					alert("경매가 준비 중입니다.");
-				}
+				// 소캣 통신 연결
+				const socket = new WebSocket('wss://i9c204.p.ssafy.io/ws/');
+				socket.addEventListener('open', () => {
+					console.log("---------------------------------")
+					console.log('WebSocket connected'); // 웹소켓 연결 확인 메시지 출력
+		
+				});
+				// 서버에 들어가자마자 openVidu 방 코드 받기
+				socket.addEventListener("message", (e) => {
 
+					const message = e.data;
+					if (message.includes("sessionId")) {
+						console.log("sessionId: ", message);
+						// 원하는 동작 수행 (navigate 등)
+					} else {
+						console.log("No message received");
+						// 메시지가 없을 때 수행할 동작 처리
+					}
+				});
+		
+				navigate("/auction/waitingroom", { state: auctionArticles })
 			}else{
 				alert("잘못된 접근입니다.")
 			}
