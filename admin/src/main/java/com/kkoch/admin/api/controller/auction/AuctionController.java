f6a23db638bcb78b71f91b807c6e0d28aea2c106
홍승준 <ghdgn22@gmail.com>
2023-08-10 21:30:14 +0900

    🏗️build: SP09P12C204-385 관리자 페이지를 위한 경매 일정 변경

--- admin/src/main/java/com/kkoch/admin/api/controller/auction/AuctionController.java
+++ admin/src/main/java/com/kkoch/admin/api/controller/auction/AuctionController.java
@@ -14,6 +14,8 @@ import com.kkoch.admin.domain.auction.Status;
 import com.kkoch.admin.login.Login;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -23,89 +25,19 @@ import java.util.List;
 import static org.springframework.http.HttpStatus.MOVED_PERMANENTLY;
 
 @RequiredArgsConstructor
-@RestController
-@RequestMapping("/admin-service/auctions")
+@Controller
+@RequestMapping("/admin-service/intranet")
 @Slf4j
 public class AuctionController {
 
     private final AuctionService auctionService;
     private final AuctionQueryService auctionQueryService;
 
-    /**
-     * 로그인을 한 관계자만 경매 일정을 등록할 수 있다.
-     *
-     * @throws IllegalArgumentException <pre>
-     *                                                                       현재 시간 + 1시간 이전의 일정이 등록되는 경우<br/>
-     *                                                                       구분코드가 범위(1 ~ 4)를 벗어나는 경우
-     *                                                                   </pre>
-     */
-    @PostMapping
-    public ApiResponse<AuctionTitleResponse> addAuction(
-            @Valid @RequestBody AddAuctionRequest request,
-            @Login LoginAdmin loginAdmin
-    ) {
-        log.info("<경매 일정 등록> Controller : 구분코드 = {}", request.getCode());
-        timeValidation(request.getStartTime());
-
-        AddAuctionDto dto = request.toAddAuctionDto();
-
-        AuctionTitleResponse response = auctionService.addAuction(loginAdmin.getId(), dto);
-        log.info("[경매 일정 등록 응답] 경매방 제목 = {}", response.getTitle());
-        return ApiResponse.ok(response);
-    }
-
-    @GetMapping("/api")
-    public ApiResponse<AuctionTitleResponse> getAuctionListForMember() {
-        log.info("<OPEN 상태의 경매 일정 조회> Controller");
-        AuctionTitleResponse openAuction = auctionQueryService.getOpenAuction();
-        log.info("<OPEN 상태의 경매 일정 조회> 경매방 이름 = {}", openAuction.getTitle());
-        return ApiResponse.ok(openAuction);
-    }
-
     @GetMapping
-    public ApiResponse<List<AuctionResponse>> getAuctions() {
+    public String getAuctions(Model model) {
         log.info("<모든 경매 일정 조회> Controller");
         List<AuctionResponse> auctionSchedule = auctionQueryService.getAuctionSchedule();
-        return ApiResponse.ok(auctionSchedule);
-    }
-
-    @PatchMapping("/{auctionId}/{status}")
-    public ApiResponse<AuctionTitleResponse> setAuctionStatus(
-            @PathVariable Long auctionId,
-            @PathVariable Status status
-    ) {
-        log.info("<경매 일정 상태 변경> Controller. 변경될 상태 = {}", status);
-        AuctionTitleResponse response = auctionService.setStatus(auctionId, status);
-
-        log.debug("[경매 일정 상태 변경] 경매방 제목 = {}", response.getTitle());
-        return ApiResponse.ok(response);
-    }
-
-    @PatchMapping("/{auctionId}")
-    public ApiResponse<AuctionTitleResponse> setAuction(
-            @PathVariable Long auctionId,
-            @Valid @RequestBody SetAuctionRequest request
-    ) {
-        log.info("<경매 일정 변경> Controller.");
-        timeValidation(request.getStartTime());
-
-        SetAuctionDto dto = request.toSetAuctionDto();
-
-        AuctionTitleResponse response = auctionService.setAuction(auctionId, dto);
-        return ApiResponse.ok(response);
-    }
-
-    @DeleteMapping("/{auctionId}")
-    public ApiResponse<Long> removeAuction(@PathVariable Long auctionId) {
-        log.info("<경매 일정 삭제> Controller.");
-        Long removedAuctionId = auctionService.remove(auctionId);
-
-        return ApiResponse.of(MOVED_PERMANENTLY, "경매 일정이 삭제되었습니다.", removedAuctionId);
-    }
-
-    private void timeValidation(LocalDateTime startTime) {
-        if (!startTime.isAfter(LocalDateTime.now().plusHours(1))) {
-            throw new IllegalArgumentException("경매 시간 입력 오류");
-        }
+        model.addAttribute("auctions",auctionSchedule);
+        return "auction";
     }
 }
