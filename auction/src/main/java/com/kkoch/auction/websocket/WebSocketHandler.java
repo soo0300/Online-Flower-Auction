0e950fb438fac45dd19780b3056f493a79ca6f31
Chaos0103 <lyt1228@naver.com>
2023-08-13 13:20:37 +0900

    ♻️refactor: S09P12C204-481 WebSocketHandler 기능 수정

--- auction/src/main/java/com/kkoch/auction/websocket/WebSocketHandler.java
+++ auction/src/main/java/com/kkoch/auction/websocket/WebSocketHandler.java
@@ -1,6 +1,11 @@
 package com.kkoch.auction.websocket;
 
+import com.kkoch.auction.api.service.AdminServiceClient;
+import com.kkoch.auction.api.service.RedisService;
+import com.kkoch.auction.api.service.dto.AuctionArticlesResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -10,11 +15,16 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.JSONObject;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
+@RequiredArgsConstructor
 @Component
 @Slf4j
 public class WebSocketHandler extends TextWebSocketHandler {
+
+    private final RedisService redisService;
+    private final AdminServiceClient adminServiceClient;
 
     private static final ConcurrentHashMap<String, WebSocketSession> sessions = new ConcurrentHashMap<>(); //웹소켓 세션 store
     private static String videoSessionId = null; //비디오 세션
@@ -23,8 +33,7 @@ public class WebSocketHandler extends TextWebSocketHandler {
     //입장
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        log.info("WebSocketHandler#afterConnectionEstablished");
-        log.info("session id = {}", session.getId());
+        log.info("connection session id = {}", session.getId());
 
         //방 생성자 외의 참가자는 videoSessionId 반환
         if (!sessions.isEmpty()) {
@@ -38,59 +47,36 @@ public class WebSocketHandler extends TextWebSocketHandler {
     //통신
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        log.info("WebSocketHandler#handleTextMessage");
-        log.info("session id = {}", session.getId());
+        log.info("message session id = {}", session.getId());
         log.info("message = {}", message);
 
-        String currentSessionId = session.getId();
-
-        //json 파싱
-        JSONParser parser = new JSONParser();
-        Object obj = parser.parse(message.getPayload());
-
-        //json 객체 변환
-        JSONObject json = (JSONObject) obj;
+        JSONObject json = getJsonObject(message);
 
         //권한 확인 추출
-        String role = (String) json.get("role");
+        String role = getDataForJson(json, "role");
         log.info("role = {}", role);
 
         //고객인데 경매방이 열리지 않은 경우
-        if (isClient(role) && isNotOpenAuction()) {
-            //세션 제거
-            sessions.remove(currentSessionId);
-            session.sendMessage(new TextMessage("not start auction"));
-            return;
+        if (isClient(role)) {
+            client(session);
         }
 
-        //관리자인 경우에만 videoSessionId 저장
         if (isAdmin(role)) {
-            adminSession = currentSessionId;
-            videoSessionId = (String) json.get("sessionId");
-            log.info("adminSession={}, videoSessionId={}", adminSession, videoSessionId);
+            admin(session, json);
         }
-
-        sessions.forEach((key, value) -> {
-            if (!key.equals(currentSessionId)) {
-                try {
-                    value.sendMessage(new TextMessage(videoSessionId));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
     }
 
     //퇴장
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        log.info("WebSocketHandler#afterConnectionClosed");
-        log.info("session id = {}", session.getId());
+        log.info("disconnection session id = {}", session.getId());
 
         String currentSessionId = session.getId();
 
         //세션 저장소에서 현재 세션 제거
         sessions.remove(currentSessionId);
+
+        log.info("currentSessionId={}, adminSession={}", currentSessionId, adminSession);
 
         //현재 세션이 관리자 세션이라면 모든 세션 종료
         if (currentSessionId.equals(adminSession)) {
@@ -99,7 +85,9 @@ public class WebSocketHandler extends TextWebSocketHandler {
             //종료 메세지 출력
             sendEndMessage();
             //모든 세션 종료
+            log.info("before session clear={}", sessions.keySet().size());
             sessions.clear();
+            log.info("after session clear={}", sessions.keySet().size());
         }
 
         //세션이 비어 있는 경우
@@ -107,12 +95,12 @@ public class WebSocketHandler extends TextWebSocketHandler {
             //비디오 세션 초기화
             videoSessionId = null;
         }
+        log.info("end adminSession = {}, videoSessionId = {}", adminSession, videoSessionId);
     }
 
     private boolean isClient(String role) {
         return role.equals("client");
     }
-
     private boolean isAdmin(String role) {
         return role.equals("admin");
     }
@@ -129,5 +117,89 @@ public class WebSocketHandler extends TextWebSocketHandler {
                 e.printStackTrace();
             }
         });
+    }
+
+    private void sendMessageAll(String data) {
+        sessions.forEach((key, value) -> {
+            try {
+                value.sendMessage(new TextMessage(data));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private JSONObject getJsonObject(TextMessage message) throws ParseException {
+        //json 파싱
+        JSONParser parser = new JSONParser();
+        Object obj = parser.parse(message.getPayload());
+
+        //json 객체 변환
+        return (JSONObject) obj;
+    }
+
+    private String getDataForJson(JSONObject json, String key) {
+        return (String) json.get(key);
+    }
+
+    private void client(WebSocketSession session) throws IOException {
+        String currentSessionId = session.getId();
+
+        if (isNotOpenAuction()) {
+            sessions.remove(currentSessionId);
+            session.sendMessage(new TextMessage("not start auction"));
+        }
+    }
+
+    private void admin(WebSocketSession session, JSONObject json) {
+        String currentSessionId = session.getId();
+        String command = getDataForJson(json, "command");
+
+        //open
+        if (command.equals("open")) {
+            log.info("open web socket");
+            adminSession = currentSessionId;
+            videoSessionId = getDataForJson(json, "sessionId");
+            log.info("adminSession={}, videoSessionId={}", adminSession, videoSessionId);
+        }
+
+        //start
+        if (command.equals("start")) {
+            log.info("auction start");
+            String test = getDataForJson(json, "auctionId");
+            log.info("auctionId = {}", test);
+
+            Long auctionId = Long.parseLong(test);
+            log.info("auctionId = {}", auctionId);
+
+            List<AuctionArticlesResponse> responses = adminServiceClient.getAuctionArticlesForAuction(auctionId).getData();
+            log.info("response size = {}", responses.size());
+
+            redisService.insertList(responses);
+            AuctionArticlesResponse nextArticle = redisService.getNextArticle();
+
+            String data = nextArticle.toJson();
+            log.info("send first json data = {}", data);
+            sendMessageAll(data);
+
+            nextArticle = redisService.getNextArticle();
+
+            data = nextArticle.toJson();
+            log.info("send second json data = {}", data);
+            sendMessageAll(data);
+        }
+
+        //next
+        if (command.equals("next")) {
+            AuctionArticlesResponse nextArticle = redisService.getNextArticle();
+            String data = nextArticle.toJson();
+            log.info("send next json data = {}", data);
+            sendMessageAll(data);
+        }
+
+        //close
+        if (command.equals("close")) {
+            log.info("close web socket");
+        }
     }
 }
