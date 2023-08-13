d19d0d76956098d18c387ca12e4cfe834daa88bb
김수진 <soo0300@naver.com>
2023-08-13 17:57:05 +0900

    ♻️refactor: S09P12C204-407 관계자 공지사항 조회 리팩토링

--- admin/src/main/java/com/kkoch/admin/api/service/notice/NoticeQueryService.java
+++ admin/src/main/java/com/kkoch/admin/api/service/notice/NoticeQueryService.java
@@ -20,7 +20,9 @@ public class NoticeQueryService {
 
     private final NoticeQueryRepository noticeQueryRepository;
 
-    public List<NoticeResponse> getAllNotices() {return noticeQueryRepository.getAllNotices(); }
+    public List<NoticeResponse> getAllNotices() {
+        return noticeQueryRepository.getAllNotices();
+    }
 
     public Page<NoticeResponse> getNotices(NoticeSearchCond cond, Pageable pageable) {
         List<NoticeResponse> content = noticeQueryRepository.getNoticeByCondition(cond, pageable);
