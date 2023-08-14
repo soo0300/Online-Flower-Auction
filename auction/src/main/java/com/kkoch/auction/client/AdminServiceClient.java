639c6393e5a590131cfbaaa2f920503208276d5b
Chaos0103 <lyt1228@naver.com>
2023-08-14 23:09:23 +0900

    🎉feat: S09P12C204-487 admin-service 통신 수정

--- auction/src/main/java/com/kkoch/auction/client/AdminServiceClient.java
+++ auction/src/main/java/com/kkoch/auction/client/AdminServiceClient.java
@@ -1,15 +1,17 @@
-package com.kkoch.auction.api.service;
+package com.kkoch.auction.client;
 
 import com.kkoch.auction.api.ApiResponse;
 import com.kkoch.auction.api.controller.request.AddTradeRequest;
 import com.kkoch.auction.api.service.dto.AuctionArticlesResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.cloud.openfeign.SpringQueryMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
+import java.util.Map;
 
 @FeignClient(name = "admin-service")
 public interface AdminServiceClient {
@@ -19,4 +21,7 @@ public interface AdminServiceClient {
 
     @PostMapping("/admin-service/trades")
     ApiResponse<Long> addTrade(@RequestBody AddTradeRequest request);
+
+    @GetMapping("/admin-service/plants/plant")
+    Long getPlantId(@SpringQueryMap Map<String, String> param);
 }
