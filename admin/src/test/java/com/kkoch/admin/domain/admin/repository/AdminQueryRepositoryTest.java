f8cc929c3bbce240332c86d546edab1c667448c0
leeyr0412 <leeyr0412@naver.com>
2023-08-15 18:27:35 +0900

    🔨test: S09P12C204-543 AdminQueryRepositoryTest 작성

--- admin/src/test/java/com/kkoch/admin/domain/admin/repository/AdminQueryRepositoryTest.java
+++ admin/src/test/java/com/kkoch/admin/domain/admin/repository/AdminQueryRepositoryTest.java
@@ -1,17 +1,16 @@
 package com.kkoch.admin.domain.admin.repository;
 
 import com.kkoch.admin.IntegrationTestSupport;
-import com.kkoch.admin.api.controller.auction.response.AuctionArticlesResponse;
+import com.kkoch.admin.api.controller.admin.LoginAdmin;
+import com.kkoch.admin.api.controller.admin.response.AdminResponse;
 import com.kkoch.admin.domain.admin.Admin;
-import com.kkoch.admin.domain.auction.Auction;
-import com.kkoch.admin.domain.plant.Category;
-import com.kkoch.admin.domain.plant.Plant;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,40 +18,52 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Transactional
 class AdminQueryRepositoryTest extends IntegrationTestSupport {
 
+    @Autowired
+    private AdminQueryRepository adminQueryRepository;
+    @Autowired
+    private AdminRepository adminRepository;
+
     @DisplayName("[관리자 목록 조회]")
     @Test
     void getAdmins() {
         //given
-        Admin.builder()
+        insertAdmin("admin1", "1234");
+        insertAdmin("admin2", "1111");
+        insertAdmin("admin3", "2222");
+        insertAdmin("admin4", "3333");
+        insertAdmin("admin5", "4444");
+
+        //when
+        List<AdminResponse> admins = adminQueryRepository.getAdmins();
+
+        //then
+        assertThat(admins).hasSize(5);
+    }
+
+    @DisplayName("[관리자  로그인]")
+    @Test
+    void getLoginAdmin() {
+        //given
+        Admin admin = insertAdmin("admin1", "1234");
+
+        //when
+        Optional<LoginAdmin> loginAdmin = adminQueryRepository.getLoginAdmin(admin.getLoginId(), admin.getLoginPw());
+
+        //then
+        assertThat(loginAdmin).isPresent();
+    }
+
+
+    private Admin insertAdmin(String id, String phone) {
+        Admin admin = Admin.builder()
                 .name("관리자")
-                .loginId("admin")
+                .loginId(id)
                 .loginPw("adminpw")
-                .tel("010-1234-5678")
+                .tel("010-1234-" + phone)
                 .active(true)
                 .position("00")
                 .build();
-
-        insertAdmin();
-
-        Plant roseFuego = insertPlant(code, rose, fuego);
-        Plant roseVictoria = insertPlant(code, rose, victoria);
-
-        Admin admin = insertAdmin();
-        Auction savedAuction1 = insertAuction(admin, LocalDateTime.of(2023, 9, 20, 5, 0));
-        Auction savedAuction2 = insertAuction(admin, LocalDateTime.of(2023, 9, 20, 5, 0));
-
-        insertAuctionArticle(roseFuego, savedAuction1, "서울", LocalDateTime.of(2023, 9, 20, 5, 0).minusDays(2));
-        insertAuctionArticle(roseFuego, savedAuction2, "광주", LocalDateTime.of(2023, 9, 20, 5, 0).minusDays(10));
-        insertAuctionArticle(roseFuego, savedAuction1, "광주", LocalDateTime.of(2023, 9, 20, 5, 0).minusDays(2));
-        insertAuctionArticle(roseVictoria, savedAuction1, "서울", LocalDateTime.of(2023, 9, 20, 5, 0).minusDays(2));
-        insertAuctionArticle(roseVictoria, savedAuction2, "광주", LocalDateTime.of(2023, 9, 20, 5, 0).minusDays(10));
-        insertAuctionArticle(roseVictoria, savedAuction1, "광주", LocalDateTime.of(2023, 9, 20, 5, 0).minusDays(2));
-
-        //when
-        List<AuctionArticlesResponse> response = auctionArticleQueryRepository.getAuctionArticleList(savedAuction1.getId());
-
-        //then
-        assertThat(response).hasSize(4);
+        return adminRepository.save(admin);
     }
 
 
