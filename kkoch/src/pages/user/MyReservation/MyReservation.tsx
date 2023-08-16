50fe2f5839436564f9f3a51aeb3fb356d2f0848b
서용준 <seoyj505@naver.com>
2023-08-16 17:53:03 +0900

    🎉feat: S09P12C204-504 경매방 관리자 연동

--- kkoch/src/pages/user/MyReservation/MyReservation.tsx
+++ kkoch/src/pages/user/MyReservation/MyReservation.tsx
@@ -24,8 +24,8 @@ const MyReservation = () => {
   function getRows() {
     axios({
       method: 'get',
-      url: `https://i9c204.p.ssafy.io/api/user-service/${secureLocalStorage.getItem('memberkey')}/reservations?pageNum=0`,
-      // url: `/api/api/user-service/${secureLocalStorage.getItem('memberkey')}/reservations?pageNum=0`,
+      // url: `https://i9c204.p.ssafy.io/api/user-service/${secureLocalStorage.getItem('memberkey')}/reservations?pageNum=0`,
+      url: `/api/api/user-service/${secureLocalStorage.getItem('memberkey')}/reservations?pageNum=0`,
       headers: {
         Authorization: `Bearer ${secureLocalStorage.getItem("token")}`
       }
